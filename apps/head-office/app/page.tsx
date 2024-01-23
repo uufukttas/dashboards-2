9484d8abd1d13163a52d7ee10288c6ebbbedc677
uufukttas <uufukttas@hotmail.com>
2024-01-23 15:57:09 +0300

    Added tailwind configuration

--- apps/head-office/app/page.tsx
+++ apps/head-office/app/page.tsx
@@ -10,20 +10,20 @@ export default async function Index() {
    * Note: The corresponding styles are in the ./index.css file.
    */
   return (
-    <div className="container w-full max-w-xs">
-      <form className="form-container bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="input-container mb-4">
-          <Label labelText="Username" htmlFor="username" className="label block text-gray-700 text-sm font-bold mb-2" />
-          <Input name="username" type="text" id="username" placeholder="Username" className="input-element shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+    <div className="w-full max-w-xs">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <Label labelText="Username" htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2" />
+          <Input name="username" type="text" id="username" placeholder="Username" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
         </div>
-        <div className="input-container mb-6">
-          <Label labelText="Password" htmlFor="password" className="label block text-gray-700 text-sm font-bold mb-2" />
-          <Input name="password" type="password" id="password" placeholder="******************" className="input-element shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
-          <p className="parapgraph-element text-red-500 text-xs italic">Please choose a password.</p>
+        <div className="mb-6">
+          <Label labelText="Password" htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2" />
+          <Input name="password" type="password" id="password" placeholder="******************" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+          <p className="text-red-500 text-xs italic">Please choose a password.</p>
         </div>
-        <div className="actions-container flex items-center justify-between">
-          <Button type="button" buttonText="Sign In" className="signin-button" />
-          <a className="forget-password inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+        <div className="flex items-center justify-between">
+          <Button type="button" buttonText="Sign In" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" />
+          <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
             Forgot Password?
           </a>
         </div>
