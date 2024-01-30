fbc29d14ecc1a789fdf35a309bf383832ec339f3
uufukttas <uufukttas@hotmail.com>
2024-01-30 08:58:46 +0300

    Reapply "Added click props to the button component"
    This reverts commit f7da96134c83e4fc7c2bc1a5cff682f433d3179e.

--- libs/button/src/lib/button.tsx
+++ libs/button/src/lib/button.tsx
@@ -3,15 +3,17 @@ export interface ButtonProps {
   buttonText: string;
   className?: string;
   type: 'button' | 'submit' | 'reset';
+  onClick?: () => void;
 }
 
 export function Button({
   className,
   type,
-  buttonText
+  buttonText,
+  onClick
 }: ButtonProps) {
   return (
-    <button className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${className}`} type={type}>
+    <button className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${className}`} type={type} onClick={onClick}>
       {buttonText}
     </button>
   );
