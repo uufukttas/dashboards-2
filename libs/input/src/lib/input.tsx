f0a8d530b186bfb89ff1f3cbec6c5b9215a8b475
uufukttas <uufukttas@hotmail.com>
2024-01-31 11:25:23 +0300

    Updated input library

--- libs/input/src/lib/input.tsx
+++ libs/input/src/lib/input.tsx
@@ -2,21 +2,21 @@
 /* eslint-disable-next-line */
 export interface InputProps {
   className?: string;
-  type: string;
-  placeholder?: string;
   id: string;
   name: string;
+  placeholder?: string;
+  type: string;
 }
 
 export function Input({
   className,
-  type,
-  placeholder,
   id,
-  name
+  name,
+  placeholder,
+  type,
 }: InputProps) {
   return (
-    <input className={`shadow appearance-none border border-[#000000]-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outlin ${className}`} id={id} type={type} placeholder={placeholder} />
+    <input className={`${className}`} id={id} name={name} type={type} placeholder={placeholder} />
 
   );
 }
