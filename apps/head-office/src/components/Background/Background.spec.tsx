32f3273a900943eea7b5cd69bad417ccd876188b
uufukttas <uufukttas@hotmail.com>
2024-01-31 14:27:10 +0300

    Add Background component unit test

---
+++ apps/head-office/src/components/Background/Background.spec.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+
+import Background from './Background';
+
+describe('Background', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Background />);
+    expect(baseElement).toBeTruthy();
+  });
+});
+``
