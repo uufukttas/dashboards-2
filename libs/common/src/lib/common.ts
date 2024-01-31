12c8fd712cf8e9d963cc97386163c457c07f9374
uufukttas <uufukttas@hotmail.com>
2024-01-31 08:38:12 +0300

    Added detectDevice function with library

---
+++ libs/common/src/lib/common.ts
@@ -0,0 +1,25 @@
+export const detectDevice = () => {
+  const deviceType = {
+    isMobile: false,
+    isTablet: false,
+    isDesktop: false,
+  }
+  const userAgent = navigator.userAgent;
+  
+  deviceType.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+  deviceType.isTablet = /iPad|Android|Tablet/i.test(userAgent);
+
+  const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const mobileWidth = 767;
+  const tabletWidth = 991;
+
+  if (deviceType.isMobile || (screenWidth <= mobileWidth)) {
+    deviceType.isMobile = true;
+  } else if (deviceType.isTablet || (screenWidth <= tabletWidth)) {
+    deviceType.isTablet = true;
+  } else {
+    deviceType.isDesktop = true;
+  }
+
+  return deviceType;
+}
