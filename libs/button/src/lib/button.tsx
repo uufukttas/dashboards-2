fda4057276f05edc38ff1d015194481fd150e109
uufukttas <uufukttas@hotmail.com>
2024-02-02 11:44:55 +0300

    Updated Button component to support children prop

--- libs/button/src/lib/button.tsx
+++ libs/button/src/lib/button.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable-next-line */
 export interface ButtonProps {
-  buttonText: string;
+  buttonText?: string;
+  children?: React.ReactNode;
   className?: string;
   type: 'button' | 'submit' | 'reset';
   onClick: (event: React.MouseEvent<HTMLElement>) => void;
 }
 
 export function Button({
+  children,
   className,
   type,
   buttonText,
@@ -14,7 +16,7 @@ export function Button({
 }: ButtonProps) {
   return (
     <button className={`button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${className}`} type={type} onClick={onClick}>
-      {buttonText}
+      {children || buttonText}
     </button>
   );
 }
