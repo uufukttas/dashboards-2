c9abdc447bc99f76298257ca315eba1aca0d171a
uufukttas <uufukttas@hotmail.com>
2024-02-02 11:45:49 +0300

    Add Header component to dashboard page

--- apps/head-office/pages/dashboards/index.tsx
+++ apps/head-office/pages/dashboards/index.tsx
@@ -4,14 +4,14 @@ import '../../src/styles/style.css'
 import { Sidebar } from '../../src/components/Sidebar/Sidebar'
 import { Provider } from 'react-redux'
 import { store } from '../../app/redux/store'
+import { Header } from '../../src/components/Header/Header'
 
 const Dashboards = () => {
   return (
     <Provider store={store}>
-      <div className='sh-dashboard-container w-full h-screen'>
-        <aside>
+      <div className='sh-dashboard-container w-full h-screen flex'>
           <Sidebar />
-        </aside>
+        <Header className={`h-[77px] flex items-center w-full`}/>
       </div>
     </Provider>
   )
