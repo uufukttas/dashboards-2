73389ad8893300acc6b54edd0209528adb0f933e
uufukttas <uufukttas@hotmail.com>
2024-02-02 09:57:25 +0300

    Added collapse&expande action to Sidebar

--- apps/head-office/src/components/Sidebar/Sidebar.tsx
+++ apps/head-office/src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
 import { LocationIcon, CloseIcon, GiftBoxIcon, PersonIcon, FAQIcon } from '@projects/icons'
 import { Image } from '@projects/image';
 import { userInfo } from '../../constants/styles';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleExpanded } from 'apps/head-office/app/redux/features/isExpanded';
+import { RootState } from 'apps/head-office/app/redux/store';
+import './Sidebar.scss';
 
 export interface SidebarProps { }
 
@@ -29,16 +32,21 @@ const sidebarElement = [
 ];
 
 export function Sidebar(props: SidebarProps) {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const isExpanded = useSelector((state: RootState) => state.expandedReducer.isExpanded);
+  const dispatch = useDispatch();
+
+  const handleClose = () => {
+    dispatch(toggleExpanded(isExpanded));
+  };
 
   return (
-    <div className={`sh-sidebar-container ${isExpanded ? 'expanded' : 'collapsed'} shadow-custom w-1/4 h-screen flex flex-col justify-between`}>
+    <div className={`sh-sidebar-container shadow-custom h-screen flex flex-col justify-between ${isExpanded !== null && (isExpanded  ? 'expanded' : 'collapsed')}`}>
 
       <div className="sh-sidebar-header flex items-center justify-between">
         <div className="sh-sidebar-header-logo mx-4">
           <Image alt={userInfo.name} src={userInfo.logo} />
         </div>
-        <div className="sh-sidebar-header-toggle mx-4 cursor-pointer" onClick={() => setIsExpanded(!isExpanded)}>
+        <div className="sh-sidebar-header-toggle mx-4 cursor-pointer" onClick={handleClose}>
           <CloseIcon />
         </div>
       </div>
@@ -46,11 +54,11 @@ export function Sidebar(props: SidebarProps) {
       <div className='sh-sidebar-content flex items-center justify-center flex-col'>
         {sidebarElement.map((item, index) => {
           return (
-            <div key={index} className="sh-sidebar-item w-full flex justify-start p-4 border border-b-0 border-r-0 ">
+            <div key={index} className="sh-sidebar-item w-full flex justify-start p-4">
               <span className="sh-sidebar-item-icon">
                 {item.icon}
               </span>
-              <span className="sh-sidebar-item-name pl-4">
+              <span className={`sh-sidebar-item-name pl-4 ${isExpanded !== null && (isExpanded ? 'block' : 'hidden')}`}>
                 {item.name}
               </span>
             </div>
