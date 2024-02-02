f99bf0f6370264df5c8cf7eb6514820e380a4fc3
uufukttas <uufukttas@hotmail.com>
2024-02-02 12:29:38 +0300

    Add Section component to Dashboards page

--- apps/head-office/pages/dashboards/index.tsx
+++ apps/head-office/pages/dashboards/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '../../app/global.css'
 import '../../src/styles/style.css'
 import { Sidebar } from '../../src/components/Sidebar/Sidebar'
+import { Section } from '../../src/components/Section/Section'
 import { Provider } from 'react-redux'
 import { store } from '../../app/redux/store'
 import { Header } from '../../src/components/Header/Header'
@@ -10,9 +11,12 @@ const Dashboards = () => {
   return (
     <Provider store={store}>
       <div className='sh-dashboard-container w-full h-screen flex'>
-          <Sidebar />
-        <Header className={`h-[77px] flex items-center w-full`}/>
-      </div>
+        <Sidebar />
+        <div className='w-full h-screen bg-white'>
+          <Header className={`h-[77px] flex items-center w-full`} />
+          <Section />
+        </div>
+      </div >
     </Provider>
   )
 }
