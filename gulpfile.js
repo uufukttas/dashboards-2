e9750779f7af787a292beba0dbbd6e68b66e8599
uufukttas <uufukttas@hotmail.com>
2024-02-05 15:15:37 +0300

    Update gulpfile for componentbase styling

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const path = require('path');
 
 function styles(projectPath) {
   return gulp.src(`${projectPath}/src/styles/style.scss`)
@@ -14,13 +15,28 @@ function watch(projectPath) {
   gulp.watch(`${projectPath}/src/styles/style.scss`, () => styles(projectPath));
 }
 
+function stylesComponents(componentPath) {
+  return gulp.src(`${componentPath}/**/*.scss`)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest(`${componentPath}`));
+}
+
+function watchComponents(componentPath) {
+  gulp.watch(`${componentPath}/**/*.scss`, () => stylesComponents(componentPath));
+}
+
 gulp.task('styles-head-office', () => styles('apps/head-office'));
 gulp.task('watch-head-office', () => watch('apps/head-office'));
+gulp.task('styles-components', () => stylesComponents('apps/head-office/src/components'));
+gulp.task('watch-components', () => watchComponents('apps/head-office/src/components'));
 
 exports.styles = styles;
 exports.watch = watch;
 
 exports.default = gulp.parallel(
   'styles-head-office',
-  'watch-head-office'
+  'watch-head-office',
+  'styles-components',
+  'watch-components'
 );
