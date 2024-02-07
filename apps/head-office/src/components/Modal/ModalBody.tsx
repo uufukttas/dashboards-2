2fbd50e46510c976bfece775088be33aac05545c
uufukttas <uufukttas@hotmail.com>
2024-02-07 15:40:01 +0300

    Updated Table component for create button

--- apps/head-office/src/components/Modal/ModalBody.tsx
+++ apps/head-office/src/components/Modal/ModalBody.tsx
@@ -22,7 +22,7 @@ const ModalBody = (props: ModalBodyProps) => {
                 <div>
                     <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
                     <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                        <option selected>Select category</option>
+                        <option defaultValue={'Select Category'}>Select category</option>
                         <option value="TV">TV/Monitors</option>
                         <option value="PC">PC</option>
                         <option value="GA">Gaming/Console</option>
@@ -35,7 +35,7 @@ const ModalBody = (props: ModalBodyProps) => {
                 </div>
             </div>
             <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                <svg className="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                <svg className="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd"></path></svg>
                 Add new product
             </button>
         </form>
