ff05401e5f1af4982716994ed474358cc7950279
uufukttas <uufukttas@hotmail.com>
2024-02-07 08:49:18 +0300

    Added InfoIcon component

--- libs/icons/src/index.ts
+++ libs/icons/src/index.ts
@@ -4,3 +4,4 @@ export { default as GiftBoxIcon } from './giftBox/giftBox';
 export { default as PersonIcon } from './person/person';
 export { default as FAQIcon } from './faq/faq';
 export { default as HamburgerMenuIcon } from './hamburgerMenu/hamburgerMenu'
+export { default as InfoIcon } from './info/info';
