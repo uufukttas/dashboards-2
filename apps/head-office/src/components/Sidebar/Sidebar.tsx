f90ecb9caf77f93f78ed556179df76ac871fa878
uufukttas <uufukttas@hotmail.com>
2024-02-07 13:43:12 +0300

    Updated Sidebar component for responsive desktop design

--- apps/head-office/src/components/Sidebar/Sidebar.tsx
+++ apps/head-office/src/components/Sidebar/Sidebar.tsx
@@ -6,6 +6,8 @@ import { toggleExpanded } from '../../../app/redux/features/isExpanded';
 import { RootState } from '../../../app/redux/store';
 import { userInfo } from '../../constants/styles';
 import './Sidebar.css';
+import { detectDevice } from '@projects/common';
+import { useState, useEffect } from 'react';
 
 export interface SidebarProps { }
 
@@ -33,56 +35,66 @@ const sidebarElement = [
 ];
 
 export function Sidebar(props: SidebarProps) {
+  const [isDetectedDevice, setIsDetectedDevice] = useState(false);
   const isExpanded = useSelector((state: RootState) => state.expandedReducer.isExpanded);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    setIsDetectedDevice(true);
+  }, []);
 
   const handleClose = () => {
     dispatch(toggleExpanded(isExpanded))
   }
 
   return (
-    <div className={`sh-sidebar-container shadow-custom h-screen flex flex-col justify-between ${isExpanded !== null ? (isExpanded ? 'expanded' : 'collapsed') : ''}`}>
+    isDetectedDevice && (
+      <div className={`sh-sidebar-container shadow-custom h-screen flex flex-col justify-between ${isExpanded !== null ? (isExpanded ? 'expanded' : 'collapsed') : ''}`}>
 
-      <div className="sh-sidebar-header flex items-center justify-between h-[77px]">
-        <div className="sh-sidebar-header-logo mx-4">
-          <Image alt={userInfo.name} src={userInfo.logo} />
+        <div className="sh-sidebar-header flex items-center justify-between h-[77px]">
+          <div className="sh-sidebar-header-logo mx-4">
+            <Image alt={userInfo.name} src={userInfo.logo} />
+          </div>
+          <div className='sh-sidebar-header-close mx-4'>
+            {
+              detectDevice().isDesktop === false &&
+              <CloseIcon onClick={handleClose} />
+            }
+          </div>
         </div>
-        <div className='sh-sidebar-header-close mx-4'>
-          <CloseIcon onClick={handleClose} />
+
+        <div className='sh-sidebar-content flex items-center justify-center flex-col'>
+          <ul className='w-full sidebar-list-wrapper'>
+            {sidebarElement.map((item, index) => {
+              return (
+                <Link href={item.link} key={index}>
+                  <li key={index} className={`cursor-pointer sidebar-list-item ${index === sidebarElement.length - 1 ? '' : 'border-b'} `}>
+                    <div className="sh-sidebar-item w-full flex justify-start p-4">
+                      <span className="sh-sidebar-item-icon">
+                        {item.icon}
+                      </span>
+                      <span className={`sh-sidebar-item-name pl-4 ${isExpanded !== null ? (isExpanded ? 'block' : 'hidden') : 'hidden'}`}>
+                        {item.name}
+                      </span>
+                    </div>
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
         </div>
+
+        <div className='sh-sidebar-footer flex items-center'>
+          <div className="sh-sidebar-footer-icon">
+            <CloseIcon />
+          </div>
+          <div className="sh-sidebar-footer-name">
+            Cikis Yap
+          </div>
+        </div>
+
       </div>
-
-      <div className='sh-sidebar-content flex items-center justify-center flex-col'>
-        <ul className='w-full sidebar-list-wrapper'>
-          {sidebarElement.map((item, index) => {
-            return (
-              <Link href={item.link} key={index}>
-                <li key={index} className={`cursor-pointer sidebar-list-item ${index === sidebarElement.length - 1 ? '' : 'border-b'} `}>
-                  <div className="sh-sidebar-item w-full flex justify-start p-4">
-                    <span className="sh-sidebar-item-icon">
-                      {item.icon}
-                    </span>
-                    <span className={`sh-sidebar-item-name pl-4 ${isExpanded !== null && (isExpanded ? 'block' : 'hidden')}`}>
-                      {item.name}
-                    </span>
-                  </div>
-                </li>
-              </Link>
-            );
-          })}
-        </ul>
-      </div>
-
-      <div className='sh-sidebar-footer flex items-center'>
-        <div className="sh-sidebar-footer-icon">
-          <CloseIcon />
-        </div>
-        <div className="sh-sidebar-footer-name">
-          Cikis Yap
-        </div>
-      </div>
-
-    </div>
+    )
   );
 }
 
