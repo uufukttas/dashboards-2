24173d0265194bbfd8b02f13a8cd6cb9b190e2b1
uufukttas <uufukttas@hotmail.com>
2024-02-08 15:51:06 +0300

    Updated ModalBody components for second fieldset

--- apps/head-office/src/components/Modal/ModalBody.tsx
+++ apps/head-office/src/components/Modal/ModalBody.tsx
@@ -53,6 +53,29 @@ const ModalBody = (props: ModalBodyProps) => {
                         </div>
                     </div>
                 </fieldset>
+
+                <fieldset data-create-form-fieldset-id={2} className={`w-full flex ${activeModal === 2 ? 'active' : 'hidden'}`}>
+                    <div className='row-wrapper w-full'>
+                        <div className='row w-full flex'>
+                            <div className='service-point-coordinate-wrapper w-1/2 px-2'>
+                                <Label htmlFor='x-coordinate' labelText='Hizmet Noktasi X Koordinati' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white' />
+                                <Input id='x-coordinate' name='x-coordinate' type='text' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 mb-4' required={true} />
+                            </div>
+                            <div className='service-point-coordinate-wrapper w-1/2 px-2'>
+                                <Label htmlFor='y-coordinate' labelText='Hizmet Noktasi Y Koordinati' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white' />
+                                <Input id='x-coordinate' name='x-coordinate' type='text' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 mb-4' required={true} />
+                            </div>
+                        </div>
+
+                        <div className='row w-full flex'>
+                            <div className='service-point-maps-wrapper w-full h-[300px] px-2'>
+                                <>
+                                Harita gelecek
+                                </>
+                            </div>
+                        </div>
+                    </div>
+                </fieldset>
             </div>
             <div className='sh-fieldset-action-button-container flex justify-between w-full float-right mt-5 relative'>
                 <Button type="button" className={`bg-blue-500 button font-bold font-medium hover:bg-blue-800 px-4 px-5 py-2 py-2.5 mx-2 rounded-lg text-black text-sm flex justify-start absolute -top-16 left-0 ${activeModal !== 1 ? 'flex' : 'hidden'}`} onClick={() => setActiveModal(activeModal - 1)}>Geri</Button>
