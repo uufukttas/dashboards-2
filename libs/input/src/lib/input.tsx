d2bccc4aca04dea23ac23b2edf81767cb5a2dc21
uufukttas <uufukttas@hotmail.com>
2024-02-08 10:42:45 +0300

    Add disabled and required props to Input component

--- libs/input/src/lib/input.tsx
+++ libs/input/src/lib/input.tsx
@@ -2,23 +2,27 @@
 /* eslint-disable-next-line */
 export interface InputProps {
   className?: string;
+  disabled?: boolean;
   id: string;
   name: string;
   placeholder?: string;
+  required?: boolean;
   type: string;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export function Input({
   className,
+  disabled,
   id,
   name,
   placeholder,
+  required,
   type,
   onChange
 }: InputProps) {
   return (
-    <input className={`input ${className}`} id={id} name={name} type={type} placeholder={placeholder} onChange={onChange}/>
+    <input className={`input ${className}`} disabled={disabled} id={id} name={name} type={type} placeholder={placeholder} required={required} onChange={onChange} />
 
   );
 }
