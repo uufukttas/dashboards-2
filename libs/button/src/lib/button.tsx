6fbb001d3734ca6248b6b8750bc6c72069da94b8
uufukttas <uufukttas@hotmail.com>
2024-02-08 14:30:13 +0300

    Remove unnecessary class in button component

--- libs/button/src/lib/button.tsx
+++ libs/button/src/lib/button.tsx
@@ -15,7 +15,7 @@ export function Button({
   onClick
 }: ButtonProps) {
   return (
-    <button className={`button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${className}`} type={type} onClick={onClick}>
+    <button className={`button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline ${className}`} type={type} onClick={onClick}>
       {children || buttonText}
     </button>
   );
