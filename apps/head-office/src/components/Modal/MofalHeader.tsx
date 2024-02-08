9aabbc3b815810893c9b5e3855ab5e8da2342128
uufukttas <uufukttas@hotmail.com>
2024-02-08 14:31:10 +0300

    Removed unused class in ModalHeader component

--- apps/head-office/src/components/Modal/MofalHeader.tsx
+++ apps/head-office/src/components/Modal/MofalHeader.tsx
@@ -16,7 +16,7 @@ const MofalHeader = ({
         dispatch(toggleVisibility(isVisible))
     }
     return (
-        <div className="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+        <div className="flex justify-between items-center border-b ">
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                 {modalHeaderTitle}
             </h3>
