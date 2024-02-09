03d58087d69228c2a96f63d282f0cd99d9bd0565
uufukttas <uufukttas@hotmail.com>
2024-02-09 10:49:10 +0300

    Refcator layout file -rfc

--- apps/head-office/app/layout.tsx
+++ apps/head-office/app/layout.tsx
@@ -1,6 +1,6 @@
+import ClientComponent from './ClientComponent';
 import './global.css';
 import '../src/styles/style.css';
-import ClientComponent from './ClientComponent';
 
 export const metadata = {
   title: 'Welcome to Sharz.net',
@@ -21,4 +21,4 @@ export default function RootLayout({
       </body>
     </html>
   );
-}
+};
