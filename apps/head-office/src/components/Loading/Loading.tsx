927540ff1c6292f24a36cb46dd6d2ecdce929b08
uufukttas <uufukttas@hotmail.com>
2024-02-09 10:47:39 +0300

    Refactor Loading componen -rfc

--- apps/head-office/src/components/Loading/Loading.tsx
+++ apps/head-office/src/components/Loading/Loading.tsx
@@ -1,14 +1,11 @@
 import './Loading.scss'
 
-export interface LoadingProps {
-}
-
-export function Header(props: LoadingProps) {
+export function Header() {
   return (
     <div className="sh-loading-container flex items-center justify-center absolute top-0 left-0 w-full h-screen opacity-70 bg-[#54565A]">
-      <div className="loading-spinner"></div>
+      <div className="sh-loading-spinner"></div>
     </div>
   );
-}
+};
 
 export default Header;
