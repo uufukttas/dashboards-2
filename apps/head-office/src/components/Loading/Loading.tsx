121d6195cc6bcd38fd1cfa9fa0d7ef28eca7cc8f
uufukttas <uufukttas@hotmail.com>
2024-02-09 11:02:44 +0300

    Preparing build scss file for loading

--- apps/head-office/src/components/Loading/Loading.tsx
+++ apps/head-office/src/components/Loading/Loading.tsx
@@ -1,9 +1,9 @@
-import './Loading.scss'
+import './Loading.css'
 
 export function Header() {
   return (
     <div className="sh-loading-container flex items-center justify-center absolute top-0 left-0 w-full h-screen opacity-70 bg-[#54565A]">
-      <div className="sh-loading-spinner"></div>
+      <div className="loading-spinner"></div>
     </div>
   );
 };
