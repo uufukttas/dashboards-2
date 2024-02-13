aaed4428cce8ff6d8551ffec738bee3099b9553f
uufukttas <uufukttas@hotmail.com>
2024-02-13 08:22:57 +0300

    Fixed code beatufier -rfc

--- apps/head-office/src/components/Section/Section.tsx
+++ apps/head-office/src/components/Section/Section.tsx
@@ -1,11 +1,10 @@
 export interface SectionProps {
   children?: React.ReactNode;
- }
+ };
 
 export function Section({
   children,
 }: SectionProps) {
-
   return (
     <section className={`sh-section-container flex-wrap w-full px-8 xl:px-20`}>
       {children}
--- libs/textarea/src/lib/textarea.tsx
+++ libs/textarea/src/lib/textarea.tsx
@@ -1,15 +1,21 @@
 /* eslint-disable-next-line */
 export interface TextareaProps {
-  placeholder?: string;
+  id: string;
   className?: string;
+  name: string;
+  placeholder?: string;
+  required: boolean;
 }
 
 export function Textarea({
+  id,
   className,
-  placeholder
+  name,
+  placeholder,
+  required,
 }: TextareaProps) {
   return (
-    <textarea id="description" rows={4} className={`block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 ${className}`} placeholder={placeholder}></textarea>
+    <textarea id={id} rows={4} className={`block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 ${className}`} name={name} placeholder={placeholder} required={required}></textarea>
   );
 }
 
