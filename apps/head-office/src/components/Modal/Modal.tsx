ba38f80a9e4eb1692a20ff81628ec5150838bd66
uufukttas <uufukttas@hotmail.com>
2024-02-26 15:18:23 +0300

    Refactor modal visibility logic in Modal and ServicePointSection components

--- apps/head-office/src/components/Modal/Modal.tsx
+++ apps/head-office/src/components/Modal/Modal.tsx
@@ -1,7 +1,5 @@
-import { useSelector } from 'react-redux';
 import ModalBody from './ModalBody';
 import ModalHeader from './MofalHeader';
-import { RootState } from '../../../app/redux/store';
 import './Modal.css';
 
 interface IModalProps {
@@ -10,10 +8,8 @@ interface IModalProps {
 };
 
 const Modal = ({ children, modalId }: IModalProps) => {
-  const isModalVisible = useSelector((state: RootState) => state.isModalVisibleReducer.isModalVisible);
-
   return (
-    <div aria-hidden='true' className={`service-point-create-modal-wrapper flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full ${isModalVisible ? 'block' : 'hidden'} bg-[#54565a33]`} id={modalId} tabIndex={-1} >
+    <div aria-hidden='true' className={`service-point-create-modal-wrapper flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full bg-[#54565a33] block`} id={modalId} tabIndex={-1} >
       <div className="service-point-create-modal-container relative p-4 bg-white rounded-lg shadow sm:p-5 relative p-4 w-full max-w-2xl h-full md:h-auto">
         <ModalHeader modalHeaderTitle={'Hizmet Noktasi Ekle'} />
         <ModalBody>
--- apps/head-office/src/components/ServicePointSection/ServicePointSection.tsx
+++ apps/head-office/src/components/ServicePointSection/ServicePointSection.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import ServicePointModalForm from './ServicePointModalForm';
 import Modal from '../Modal/Modal';
 import Table from '../Table/Table';
+import { RootState } from '../../../app/redux/store';
 
 export function ServicePointSection() {
+  const isModalVisible = useSelector((state: RootState) => state.isModalVisibleReducer.isModalVisible);
+
   return (
     <div className={`service-point-container flex justify-between items-center pt-6 md:pt-12 flex-col`}>
-      <Modal modalId={'service-point-create-modal'}>
-        <ServicePointModalForm />
-      </Modal>
       <div className="service-point-table-container flex items-center w-full">
         <Table />
       </div>
+      {
+        isModalVisible &&
+        <Modal modalId={'service-point-create-modal'}>
+          <ServicePointModalForm />
+        </Modal>
+      }
     </div>
   );
 };
