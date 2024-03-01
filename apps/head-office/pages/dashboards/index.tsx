3188f5e35dc3abfcf3838fcf16a0391f6e1bbfe5
uufukttas <uufukttas@hotmail.com>
2024-03-01 11:36:53 +0300

    Refactor code and remove unused imports for dashboards page -rfc

--- apps/head-office/pages/dashboards/index.tsx
+++ apps/head-office/pages/dashboards/index.tsx
@@ -1,22 +1,12 @@
-import React from 'react'
-import Head from 'next/head'
-import { Provider } from 'react-redux'
-import Card from '../../src/components/Card/Card';
-import MainPage from '../../src/components/MainPage/MainPage'
-import { store } from '../../app/redux/store'
-import '../../app/global.css'
-import '../../src/styles/style.css'
+import React from 'react';
+import Head from 'next/head';
+import { Provider } from 'react-redux';
+import MainPage from '../../src/components/MainPage/MainPage';
+import { store } from '../../app/redux/store';
+import '../../app/global.css';
+import '../../src/styles/style.css';
 
 const Dashboards = () => {
-  const cardBodyChildren = (
-    <>
-      <div className='flex justify-center items-center text-center pb-4'>Aktif Kullanicilar </div>
-      <div className='flex justify-center items-center border rounded-full p-4 bg-blue-500 text-white'>
-        <h2 className='text-sm flex justify-center items-end'><span className='text-4xl'>8</span>%</h2>
-      </div>
-    </>
-  );
-
   return (
     <>
       <Head>
@@ -26,17 +16,12 @@ const Dashboards = () => {
         <div className='sh-dashboard-page-container w-full h-screen flex'>
           <MainPage>
             <div className='flex justify-center items-center pt-12 flex-wrap'>
-              <Card
-                cardBodyChildren={cardBodyChildren}
-                className={'w-full xl:w-3/4 my-8 '}
-                onClick={() => { }}
-              />
             </div>
           </MainPage>
         </div >
       </Provider>
     </>
-  )
-}
+  );
+};
 
-export default Dashboards
+export default Dashboards;
