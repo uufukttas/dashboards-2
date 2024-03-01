4431adb41424327f1cca96784eb0a918fa737126
uufukttas <uufukttas@hotmail.com>
2024-03-01 11:34:07 +0300

    Refactor Icon component -rfc

--- libs/icons/src/person/person.tsx
+++ libs/icons/src/person/person.tsx
@@ -1,9 +1,5 @@
-interface IPersonIconProps {
-  strokeColor: string;
-}
-
-const PersonIcon = ({ strokeColor }: IPersonIconProps) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke={strokeColor} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className='-mt-2'>
+const PersonIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className='-mt-2'>
     <circle cx="12" cy="12" r="5"></circle>
     <ellipse cx="12" cy="21" rx="8" ry="5"></ellipse>
   </svg>
