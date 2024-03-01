8937573b021ac1738bc1b4890ec9c81ec3e1f351
uufukttas <uufukttas@hotmail.com>
2024-03-01 10:08:32 +0300

    Refactor button component to handle children prop

--- libs/button/src/lib/button.tsx
+++ libs/button/src/lib/button.tsx
@@ -5,7 +5,7 @@ interface IButtonProps {
   id?: string;
   type: 'button' | 'submit' | 'reset';
   onClick?: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
-}
+};
 
 export function Button({
   buttonText,
@@ -13,13 +13,17 @@ export function Button({
   className,
   id,
   type,
-  onClick
+  onClick,
 }: IButtonProps) {
   return (
-    <button className={`button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline ${className}`} id={id} type={type} onClick={onClick}>
-      {children || buttonText}
+    <button
+      className={`button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline ${className}`}
+      id={id}
+      type={type}
+      onClick={onClick}>
+      {buttonText ? buttonText : children }
     </button>
   );
-}
+};
 
 export default Button;
