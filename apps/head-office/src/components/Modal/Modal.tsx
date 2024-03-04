e4498c463802f61fc0b5f55b419598ba0cb20284
uufukttas <uufukttas@hotmail.com>
2024-03-04 14:19:33 +0300

    Updated Modal Header

--- apps/head-office/src/components/Modal/Modal.tsx
+++ apps/head-office/src/components/Modal/Modal.tsx
@@ -1,5 +1,7 @@
+import { useSelector } from 'react-redux';
 import ModalBody from './ModalBody';
 import ModalHeader from './MofalHeader';
+import { RootState } from '../../../app/redux/store';
 
 interface IModalProps {
   children: React.ReactNode;
@@ -7,10 +9,12 @@ interface IModalProps {
 };
 
 const Modal = ({ children, modalId }: IModalProps) => {
+  const updatedServicePointData = useSelector((state: RootState) => state.updatedServicePointData.updatedServicePointData);
+
   return (
     <div aria-hidden='true' className={`sh-service-point-modal-container flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full bg-[#54565a33] block`} id={modalId} tabIndex={-1} >
       <div className="sh-service-point-modal-wrapper relative p-4 bg-white rounded-lg shadow sm:p-5 relative p-4 w-full max-w-2xl h-full md:h-auto">
-        <ModalHeader modalHeaderTitle={'Hizmet Noktasi Ekle'} />
+        <ModalHeader modalHeaderTitle={`Hizmet Noktası ${updatedServicePointData.id > 0 ? 'Güncelle' : 'Ekle'}`} />
         <ModalBody>
           {children}
         </ModalBody>
