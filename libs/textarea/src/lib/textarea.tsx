342fd52b3477f34f69f8911849f31e1785903f8c
uufukttas <uufukttas@hotmail.com>
2024-03-05 11:52:51 +0300

    Refactor textarea component class names -rfc

--- libs/textarea/src/lib/textarea.tsx
+++ libs/textarea/src/lib/textarea.tsx
@@ -22,7 +22,7 @@ export function Textarea({
   onChange,
 }: ITextareaProps) {
   return (
-    <textarea id={id} rows={4} className={`block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 ${className}`} name={name} placeholder={placeholder}  required={required} value={value} onChange={onChange} {...register}></textarea>
+    <textarea id={id} rows={4} className={`text-area ${className}`} name={name} placeholder={placeholder}  required={required} value={value} onChange={onChange} {...register}></textarea>
   );
 }
 
