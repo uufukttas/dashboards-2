4f3b8bd1728a3ded68c33f9b158b59aeb74fb2ba
uufukttas <uufukttas@hotmail.com>
2024-03-06 15:36:26 +0300

    Add ServicePointInfoPage component for every service point

---
+++ apps/head-office/pages/service-points/service-point/[...slug].tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { store } from '../../../app/redux/store';
+import MainPage from '../../../src/components/MainPage/MainPage';
+import '../../../app/global.css';
+import '../../../src/styles/style.css';
+
+const ServicePointInfoPage = () => {
+  
+  return (
+    <>
+      <Head>
+        <title>Dashboards | Sharz.net</title>
+      </Head>
+      <Provider store={store}>
+        <div className='sh-dashboard-page-container w-full h-screen flex'>
+          <MainPage>
+            <div className='flex justify-center items-center pt-12 flex-wrap'>
+
+            </div>
+          </MainPage>
+        </div >
+      </Provider>
+    </>
+  );
+};
+
+export default ServicePointInfoPage;
+
