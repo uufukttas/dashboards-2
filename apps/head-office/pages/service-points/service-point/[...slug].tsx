f03e8919c85a016a2dea98b051d93dc366cf72c4
uufukttas <uufukttas@hotmail.com>
2024-03-07 13:27:22 +0300

    Add slug page for ServicePointDetailsPage

--- apps/head-office/pages/service-points/service-point/[...slug].tsx
+++ apps/head-office/pages/service-points/service-point/[...slug].tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { Provider } from 'react-redux';
 import { store } from '../../../app/redux/store';
+import ServicePointDetailsPage from './ServicePointsDetailsPage'
 import MainPage from '../../../src/components/MainPage/MainPage';
 import '../../../app/global.css';
 import '../../../src/styles/style.css';
 
+
 const ServicePointInfoPage = () => {
-  
+  const router = useRouter();
+  const slug = router.query?.slug;
+
   return (
     <>
       <Head>
-        <title>Dashboards | Sharz.net</title>
+        <title>{`Service Point Details - ${router.query.slug} | Sharz.net`}</title>
       </Head>
       <Provider store={store}>
-        <div className='sh-dashboard-page-container w-full h-screen flex'>
+        <div className="sh-service-point-detail-page-container w-full h-screen flex">
           <MainPage>
-            <div className='flex justify-center items-center pt-12 flex-wrap'>
-
+            <div className="service-point-page-wrapper flex justify-center items-center md:pt-12 flex-wrap w-full">
+              {
+                slug &&
+                <ServicePointDetailsPage slug={slug[0]}/>
+              }
             </div>
           </MainPage>
         </div >
