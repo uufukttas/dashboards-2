a4433555d3b3eaba683c0b10bf509b3307a67b6d
uufukttas <uufukttas@hotmail.com>
2024-03-08 12:15:43 +0300

    Refactor service point detail page layout

--- apps/head-office/pages/service-points/service-point/[...slug].tsx
+++ apps/head-office/pages/service-points/service-point/[...slug].tsx
@@ -10,14 +10,13 @@ import { BRAND_PREFIX } from '../../../src/constants/constants';
 import '../../../app/global.css';
 import '../../../src/styles/style.css';
 
-
 const ServicePointInfoPage = () => {
   const router = useRouter();
   const slug = router.query?.slug;
 
   const workingHoursContent = (
     <div className="working-hours-content py-8">
-    Bu servis istasyonunun calisma saatleri 08:00 - 18:00 arasindadir.
+      Bu servis istasyonunun calisma saatleri 08:00 - 18:00 arasindadir.
     </div>
   );
 
@@ -27,24 +26,29 @@ const ServicePointInfoPage = () => {
         <title>{`Service Point Details - ${router.query.slug} | Sharz.net`}</title>
       </Head>
       <Provider store={store}>
-        <div className={`${BRAND_PREFIX}-service-point-detail-page-container w-full h-screen flex`}>
+        <div
+          className={`${BRAND_PREFIX}-service-point-detail-page-container w-full h-screen flex`}
+        >
           <MainPage>
             <div className="service-point-page-wrapper flex justify-center items-center md:pt-12 flex-wrap w-full ">
               {
                 slug && (
                   <>
                     <ServicePointDetailsPage slug={slug[0]} />
-                    <Accordion accordionTitle={'Calisma Saatleri'} accordionContent={workingHoursContent} className={`mx-8 rounded-lg`}/>
+                    <Accordion
+                      accordionTitle={'Calisma Saatleri'}
+                      accordionContent={workingHoursContent}
+                      className={`mx-8 rounded-lg`}
+                    />
                   </>
                 )
               }
             </div>
           </MainPage>
-        </div >
+        </div>
       </Provider>
     </>
   );
 };
 
 export default ServicePointInfoPage;
-
