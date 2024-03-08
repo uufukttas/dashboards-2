1a1a2598807890b6bd6e470871d00c571c19754f
uufukttas <uufukttas@hotmail.com>
2024-03-08 10:52:26 +0300

    Refactor Modal component to include dynamic modal header title -rfc

--- apps/head-office/src/components/Modal/Modal.tsx
+++ apps/head-office/src/components/Modal/Modal.tsx
@@ -1,26 +1,24 @@
-import { useSelector } from 'react-redux';
 import ModalBody from './ModalBody';
 import ModalHeader from './MofalHeader';
 import { BRAND_PREFIX } from '../../constants/constants';
-import { RootState } from '../../../app/redux/store';
 
 interface IModalProps {
   children: React.ReactNode;
+  modalHeaderTitle: string;
   modalId: string;
 };
 
-const Modal = ({ children, modalId }: IModalProps) => {
-  const updatedServicePointData = useSelector((state: RootState) => state.updatedServicePointData.updatedServicePointData);
-
+const Modal = ({ children, modalId, modalHeaderTitle }: IModalProps) => {
   return (
     <div
-      aria-hidden='true'
       className={`${BRAND_PREFIX}-service-point-modal-container flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-40 justify-center items-center w-full md:inset-0 bg-[#54565a33] block h-full`}
       id={modalId}
       tabIndex={-1}
     >
       <div className={`${BRAND_PREFIX}-service-point-modal-wrapper relative p-4 bg-white rounded-lg shadow sm:p-5 relative p-4 w-full max-w-2xl sm:h-auto`}>
-        <ModalHeader modalHeaderTitle={`Hizmet Noktası ${updatedServicePointData.id > 0 ? 'Güncelle' : 'Ekle'}`} />
+        <ModalHeader
+          modalHeaderTitle={modalHeaderTitle}
+        />
         <ModalBody>
           {children}
         </ModalBody>
--- apps/head-office/src/components/Table/Table.tsx
+++ apps/head-office/src/components/Table/Table.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from '@projects/button';
-import { InfoIcon, PenIcon, TrashIcon } from '@projects/icons';
+import { InfoIcon, PenIcon, SearchIcon, TrashIcon } from '@projects/icons';
 import { Input } from '@projects/input';
 import { Label } from '@projects/label';
 import { BRAND_PREFIX, CITIES, DISTRICTS } from '../../constants/constants';
@@ -44,28 +44,123 @@ interface IServicePointProps {
 
 export function Table() {
   const isModalVisible = useSelector((state: RootState) => state.isModalVisible);
-  const [servicePoints, setServicePoints] = useState([]);
   const [isHidden, setIsHidden] = useState(true);
   const [selectedRow, setSelectedRow] = useState(0);
+  const [servicePoints, setServicePoints] = useState([]);
   const dispatch = useDispatch();
 
-  const handleClick = (e: React.MouseEvent) => {
-    dispatch(toggleModalVisibility(isModalVisible))
+  const createTableRow = ({ servicePoint }: IServicePointInfoProps) => {
+    return (
+      <Fragment key={servicePoint.id}>
+        <tr data-service-point-id={servicePoint.id}>
+          <td className="px-6 py-3">{servicePoint.name}</td>
+          <td className="px-6 py-3">{servicePoint.type}</td>
+          <td className="px-6 py-3">{servicePoint.phone}</td>
+          <td className="px-6 py-3">{servicePoint.address}</td>
+          <td className="px-6 py-3">{getCity((servicePoint.cityId))}</td>
+          <td className="px-6 py-3">{getDistricts(servicePoint.districtId)}</td>
+          <td className="px-6 py-4 items-center w-full justify-center flex">
+            <a
+              className="font-medium text-blue-600 cursor-pointer px-2"
+              data-modal-show="editUserModal"
+              data-service-point-id={servicePoint.id}
+              onClick={getUpdatedServicePointsInfo}
+            >
+              <PenIcon />
+            </a>
+            <a
+              className="font-medium text-red-600 cursor-pointer px-2"
+              data-modal-show="deleteUserModal"
+              data-service-point-id={servicePoint.id}
+              onClick={deleteServicePointInfo}
+            >
+              <TrashIcon />
+            </a>
+            <Link className='px-2' href={`/service-points/service-point/${servicePoint.id}`}>
+              <InfoIcon />
+            </Link>
+            <Button
+              className="font-medium text-blue-600 px-2"
+              data-modal-show="editUserModal"
+              type="button"
+              onClick={() => { toggleTableRow(servicePoint.id) }}
+            >
+              <div
+                className='text-lg'
+                dangerouslySetInnerHTML={{ __html: `${servicePoint.id === selectedRow ? '&#11205;' : '&#11206;'}` }}
+              />
+            </Button>
+          </td>
+        </tr>
+        {servicePoint.id === selectedRow && (
+          <>
+            <tr className='bg-gray-50'>
+              <th className='px-6'>Longitude</th>
+              <th className='px-6'>Latitude</th>
+              <th className='px-6'>Payment Methods</th>
+              <th className='px-6'>Free Park</th>
+              <th className='px-6'>Opportunuties</th>
+              <th className='px-6'> </th>
+              <th className='px-6'> </th>
+            </tr>
+            <tr>
+              <td className='px-6 py-3'>{servicePoint.longitude}</td>
+              <td className='px-6 py-3'>{servicePoint.latitude}</td>
+            </tr>
+          </>
+        )}
+        <tr>
+          <td colSpan={8} style={{ height: "2px", backgroundColor: "#ececec" }}></td>
+        </tr>
+      </Fragment>
+    )
   };
-  const toggleTableRow = (id: number) => {
-    setIsHidden(!isHidden);
+  const deleteServicePointInfo = async (event: React.MouseEvent<HTMLAnchorElement>) => {
+    const servicePointIdAttr = event.currentTarget.getAttribute('data-service-point-id');
+    const servicePointId = servicePointIdAttr ? parseInt(servicePointIdAttr) : NaN;
 
-    if (selectedRow !== id) {
-      setSelectedRow(id);
-      return;
+    try {
+      await axios.post(
+        process.env.DELETE_STATION_URL || '', ({
+          'id': servicePointId
+        }))
+        .then((response) => response.data)
+        .then(response => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+    } catch (error) {
+      console.error(error);
     }
+  }
+  const getCity = (rid: number) => {
+    return CITIES[rid?.toString()];
+  };
+  const getDistricts = (districtCode: number) => {
+    return DISTRICTS[districtCode?.toString()];
+  };
+  const getFirstTenUsers = async () => {
+    try {
+      await axios.post(
+        (process.env.GET_ALL_SERVICE_POINTS || ''),
+        ({
+          'pageNumber': 1,
+          'pageSize': 10
+        })
+      )
+        .then((response) => response.data)
+        .then(response => {
+          const filteredArr = response.data.filter((servicePoint: IServicePointProps) => servicePoint.isActive);
+          setServicePoints(filteredArr);
+        })
+        .catch((error) => console.log(error));
 
-    if (!isHidden) {
-      setSelectedRow(0);
-      return;
+    } catch (error) {
+      console.error(error);
     }
-
-    setSelectedRow(id);
   };
   const getUpdatedServicePointsInfo = async (event: React.MouseEvent<HTMLAnchorElement>) => {
     const servicePointIdAttr = event.currentTarget.getAttribute('data-service-point-id') || '0';
@@ -101,103 +196,23 @@ export function Table() {
       console.error(error);
     }
   };
-  const getFirstTenUsers = async () => {
-    try {
-      await axios.post(
-        (process.env.GET_ALL_SERVICE_POINTS || ''),
-        ({
-          'pageNumber': 1,
-          'pageSize': 10
-        })
-      )
-        .then((response) => response.data)
-        .then(response => {
-          const filteredArr = response.data.filter((servicePoint: IServicePointProps) => servicePoint.isActive);
-          setServicePoints(filteredArr);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+  const handleClick = (e: React.MouseEvent) => {
+    dispatch(toggleModalVisibility(isModalVisible))
+  };
+  const toggleTableRow = (id: number) => {
+    setIsHidden(!isHidden);
 
-    } catch (error) {
-      console.error(error);
+    if (selectedRow !== id) {
+      setSelectedRow(id);
+      return;
     }
-  };
-  const getCity = (rid: number) => {
-    return CITIES[rid?.toString()];
-  };
-  const getDistricts = (districtCode: number) => {
-    return DISTRICTS[districtCode?.toString()];
-  };
-  const deleteServicePointInfo = async (event: React.MouseEvent<HTMLAnchorElement>) => {
-    const servicePointIdAttr = event.currentTarget.getAttribute('data-service-point-id');
-    const servicePointId = servicePointIdAttr ? parseInt(servicePointIdAttr) : NaN;
 
-    try {
-      await axios.post(
-        process.env.DELETE_STATION_URL || '', ({
-          'id': servicePointId
-        }))
-        .then((response) => response.data)
-        .then(response => {
-          console.log(response);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-    } catch (error) {
-      console.error(error);
+    if (!isHidden) {
+      setSelectedRow(0);
+      return;
     }
-  }
 
-  const createTableRow = ({ servicePoint }: IServicePointInfoProps) => {
-    return (
-      <Fragment key={servicePoint.id}>
-        <tr data-service-point-id={servicePoint.id}>
-          <td className='px-6 py-3'>{servicePoint.name}</td>
-          <td className='px-6 py-3'>{servicePoint.type}</td>
-          <td className='px-6 py-3'>{servicePoint.phone}</td>
-          <td className='px-6 py-3'>{servicePoint.address}</td>
-          <td className='px-6 py-3'>{getCity((servicePoint.cityId))}</td>
-          <td className='px-6 py-3'>{getDistricts(servicePoint.districtId)}</td>
-          <td className="px-6 py-4 items-center w-full justify-center flex">
-            <a data-modal-show="editUserModal" data-service-point-id={servicePoint.id} className="font-medium text-blue-600 cursor-pointer px-2" onClick={getUpdatedServicePointsInfo}>
-              <PenIcon />
-            </a>
-            <a data-modal-show="deleteUserModal" data-service-point-id={servicePoint.id} className="font-medium text-red-600 cursor-pointer px-2" onClick={deleteServicePointInfo}>
-              <TrashIcon />
-            </a>
-            <Link className='px-2' href={`/service-points/service-point/${servicePoint.id}`}>
-              <InfoIcon />
-            </Link>
-            <Button type="button" data-modal-show="editUserModal" className="font-medium text-blue-600 px-2" onClick={() => { toggleTableRow(servicePoint.id) }}>
-              <div className='text-lg' dangerouslySetInnerHTML={{ __html: `${servicePoint.id === selectedRow ? '&#11205;' : '&#11206;'}` }} />
-            </Button>
-          </td>
-        </tr>
-        {servicePoint.id === selectedRow && (
-          <>
-            <tr className='bg-gray-50'>
-              <th className='px-6'>Longitude</th>
-              <th className='px-6'>Latitude</th>
-              <th className='px-6'>Payment Methods</th>
-              <th className='px-6'>Free Park</th>
-              <th className='px-6'>Opportunuties</th>
-              <th className='px-6'> </th>
-              <th className='px-6'> </th>
-            </tr>
-            <tr>
-              <td className='px-6 py-3'>{servicePoint.longitude}</td>
-              <td className='px-6 py-3'>{servicePoint.latitude}</td>
-            </tr>
-          </>
-        )}
-        <tr>
-          <td colSpan={8} style={{ height: "2px", backgroundColor: "#ececec" }}></td>
-        </tr>
-      </Fragment>
-    )
+    setSelectedRow(id);
   };
 
   useEffect(() => {
@@ -211,21 +226,25 @@ export function Table() {
     <div className={`${BRAND_PREFIX}-table-container relative overflow-x-auto shadow-md sm:rounded-lg max-w-[330px] md:max-w-full w-full`}>
       <div className={`${BRAND_PREFIX}-table-wrapper flex flex-col items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 bg-white w-full md:flex-row`}>
         <div className={`${BRAND_PREFIX}-table-actions w-4/5 md:w-1/6`}>
-          <Button className={`${BRAND_PREFIX}-add-service-point-button-container w-full`} type="button" onClick={handleClick}>+ Hizmet Noktasi</Button>
+          <Button
+            className={`${BRAND_PREFIX}-add-service-point-button-container w-full`}
+            type="button"
+            onClick={handleClick}
+          >
+            + Hizmet Noktasi
+          </Button>
         </div>
         <Label className="sr-only" htmlFor="table-search" labelText={`Search`} />
         <div className={`${BRAND_PREFIX}-service-point-search-input-container relative w-4/5 md:w-1/6 mx-2`}>
           <div className={`${BRAND_PREFIX}-service-point-search-icon-container absolute inset-y-0 flex items-center ps-3 pointer-events-none pl-3 pr-2 justify-end border-r`}>
-            <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-            </svg>
+            <SearchIcon />
           </div>
           <Input
-          className={`${BRAND_PREFIX}-service-point-search-input w-full block p-2 md:mx-2 pl-10 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500`}
-          id="table-search-input"
-          name="search"
-          placeholder="Search users"
-          type="text"
+            className={`${BRAND_PREFIX}-service-point-search-input w-full block p-2 md:mx-2 pl-10 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500`}
+            id="table-search-input"
+            name="search"
+            placeholder="Search users"
+            type="text"
           />
         </div>
       </div>
