b8f12566e2105224827caefcb28d963b38187b2c
uufukttas <uufukttas@hotmail.com>
2024-03-11 16:30:20 +0300

    Added chargeUnit to the details page

--- apps/head-office/pages/service-points/service-point/ServicePointsDetailsPage.tsx
+++ apps/head-office/pages/service-points/service-point/ServicePointsDetailsPage.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Accordion from '../../../src/components/Accordion/Accordion';
 import Card from '../../../src/components/Card/Card';
 import { CITIES, DISTRICTS } from '../../../src/constants/constants';
+import { Button } from '@projects/button';
 
 interface IServicePointsDetailsPageProps {
     slug: string;
@@ -28,7 +30,25 @@ interface IServicePointsDetailsInfoProps {
     lon: number;
     cityId: number;
     districtId: number;
-}
+};
+
+interface IChargeUnitsProps {
+    chargePointId: number;
+    connectorNumber: number;
+    count: number;
+    deviceCode: string;
+    externalAddress: string;
+    internalAddress: string;
+    investor: string;
+    isFreePoint: boolean;
+    lastHeartBeat: string;
+    limitedUsage: boolean;
+    model: string;
+    ocppVersion: number;
+    sendRoaming: boolean;
+    stationId: number;
+    status: string;
+};
 
 const initialServicePointsDetailsStateValue = {
     name: '',
@@ -51,11 +71,41 @@ const initialServicePointsDetailsInfoStateValue = {
     lon: 0,
     cityId: 0,
     districtId: 0
-}
+};
+
+const initialChargeUnitsStateValue = {
+    chargePointId: 2026,
+    connectorNumber: 1,
+    count: 1,
+    deviceCode: "9081000201",
+    externalAddress: '',
+    internalAddress: '',
+    investor: "Operatör",
+    isFreePoint: false,
+    lastHeartBeat: '',
+    limitedUsage: false,
+    model: "Gersan",
+    ocppVersion: 1500,
+    sendRoaming: true,
+    stationId: 2022,
+    status: "Kullanılabilir",
+};
 
 const ServicePointsDetailsPage = ({ slug }: IServicePointsDetailsPageProps) => {
     const [servicePointDetails, setServicePointDetails] = useState<IServicePointsDetailsProps>(initialServicePointsDetailsStateValue);
     const [servicePointDetailsInfo, setServicePointDetailsInfo] = useState<IServicePointsDetailsInfoProps>(initialServicePointsDetailsInfoStateValue);
+    const [chargeUnits, setChargeUnits] = useState<IChargeUnitsProps[]>([initialChargeUnitsStateValue]);
+
+    const getChargeUnits = () => {
+        axios.post(
+            'https://sharztestapi.azurewebsites.net/ServicePoint/GetStationSettings',
+            JSON.stringify({ stationId: Number(slug), PageNumber: 1, PageSize: 5 }),
+            { headers: { 'Content-Type': 'application/json' } }
+        )
+            .then(response => response.data.data)
+            .then(data => setChargeUnits(data))
+            .catch(error => console.log(error));
+    };
 
     const getServicePointsDetails = async (slug: string) => {
         axios.post(
@@ -87,6 +137,39 @@ const ServicePointsDetailsPage = ({ slug }: IServicePointsDetailsPageProps) => {
         return DISTRICTS[districtId?.toString()];
     };
 
+    const chargeUnitsContent = (
+        <div className="charge-units-content py-8">
+            <div className="charge-units-header flex justify-end">
+                {/* <h3 className="charge-units-title text-xl font-bold">Sarj Üniteleri</h3> */}
+                <Button className="charge-units-add-button bg-primary text-white rounded px-4 py-2 text-2xl tex-center item-center" type="button">
+                    <span>+</span>
+                </Button>
+            </div>
+            <div className="charge-units-list">
+                {
+                    chargeUnits.map((chargeUnit, index) => (
+                        <div key={index} className="charge-unit flex justify-between items-center border-b-2 border-gray-200 py-4">
+                            <div className="charge-unit-info">
+                                <h3 className="charge-unit-name text-lg font-bold">{chargeUnit.deviceCode}</h3>
+                                <p className="charge-unit-status text-sm">{chargeUnit.status}</p>
+                            </div>
+                            <div className="charge-unit-actions mx-2">
+                                <button className="charge-unit-edit-button bg-primary text-white rounded-md px-4 py-2 mx-2">Düzenle</button>
+                                <button className="charge-unit-delete-button bg-secondary text-white rounded-md px-4 py-2 mx-2">Sil</button>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
+
+    const workingHoursContent = (
+        <div className="working-hours-content py-8">
+            Bu servis istasyonunun calisma saatleri 08:00 - 18:00 arasindadir.
+        </div>
+    );
+
     const cardBody = (
         <>
             <div className='w-full flex itemse-center justify-between'>
@@ -110,20 +193,20 @@ const ServicePointsDetailsPage = ({ slug }: IServicePointsDetailsPageProps) => {
                         {
                             servicePointDetails.isActive
                                 ? <div className='bg-green-500 rounded-full h-4 w-4 mx-2'></div>
-                                : <div className='bg-red-500 rounded-full h-4 w-4 mx-2'></div>
+                                : <div className='bg-secondary rounded-full h-4 w-4 mx-2'></div>
                         }
                     </div>
                     <div className='flex justify-end items-baseline'>
                         <p className='px-2'>Sehir </p>
-                        <h1 className='text-2xl'>{getSelectedCity(servicePointDetailsInfo.cityId)}</h1>
+                        <h1 className='text-2xl'>{getSelectedCity(servicePointDetailsInfo?.cityId)}</h1>
                     </div>
                     <div className='flex justify-end items-baseline'>
                         <p className='px-2'>Ilce </p>
-                        <h1 className='text-2xl'>{getSelectedDistrict(servicePointDetailsInfo.districtId)}</h1>
+                        <h1 className='text-2xl'>{getSelectedDistrict(servicePointDetailsInfo?.districtId)}</h1>
                     </div>
                     <div className='flex justify-end items-baseline'>
                         <p className='px-2'>Adres </p>
-                        <h1 className='text-2xl flex-wrap'>{servicePointDetailsInfo.address}</h1>
+                        <h1 className='text-2xl flex-wrap'>{servicePointDetailsInfo?.address}</h1>
                     </div>
                 </div>
             </div>
@@ -131,15 +214,29 @@ const ServicePointsDetailsPage = ({ slug }: IServicePointsDetailsPageProps) => {
     );
 
     useEffect(() => {
+        getChargeUnits();
         getServicePointsDetails(slug[0]);
         getServicePointsDetailsInfo(slug[0]);
     }, [slug])
 
     return (
-        servicePointDetailsInfo.cityId !== 0 && servicePointDetailsInfo.districtId !== 0 &&
-        <div className='w-full'>
-            <Card cardBodyChildren={cardBody} className='my-5' />
-        </div>
+        servicePointDetailsInfo?.cityId !== 0 && servicePointDetailsInfo?.districtId !== 0 &&
+        <>
+            <div className='w-full'>
+                <Card cardBodyChildren={cardBody} className='my-5' />
+            </div>
+            <Accordion
+                accordionTitle={'Sarj Üniteleri'}
+                accordionContent={chargeUnitsContent}
+                className={`mx-8 my-8 rounded-lg`}
+            />
+            <Accordion
+                accordionTitle={'Calisma Saatleri'}
+                accordionContent={workingHoursContent}
+                className={`mx-8 my-8 rounded-lg`}
+            />
+        </>
+
     )
 }
 
--- apps/head-office/pages/service-points/service-point/[...slug].tsx
+++ apps/head-office/pages/service-points/service-point/[...slug].tsx
@@ -4,7 +4,6 @@ import { useRouter } from 'next/router';
 import { Provider } from 'react-redux';
 import { store } from '../../../app/redux/store';
 import ServicePointDetailsPage from './ServicePointsDetailsPage';
-import Accordion from '../../../src/components/Accordion/Accordion';
 import MainPage from '../../../src/components/MainPage/MainPage';
 import { BRAND_PREFIX } from '../../../src/constants/constants';
 import '../../../app/global.css';
@@ -13,12 +12,6 @@ import '../../../src/styles/style.css';
 const ServicePointInfoPage = () => {
   const router = useRouter();
   const slug = router.query?.slug;
-
-  const workingHoursContent = (
-    <div className="working-hours-content py-8">
-      Bu servis istasyonunun calisma saatleri 08:00 - 18:00 arasindadir.
-    </div>
-  );
 
   return (
     <>
@@ -35,16 +28,6 @@ const ServicePointInfoPage = () => {
                 slug && (
                   <>
                     <ServicePointDetailsPage slug={slug[0]} />
-                    <Accordion
-                      accordionTitle={'Sarj Üniteleri'}
-                      accordionContent={workingHoursContent}
-                      className={`mx-8 my-8 rounded-lg`}
-                    />
-                    <Accordion
-                      accordionTitle={'Calisma Saatleri'}
-                      accordionContent={workingHoursContent}
-                      className={`mx-8 my-8 rounded-lg`}
-                    />
                   </>
                 )
               }
