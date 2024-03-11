6727fec808d20a9e74ac001c4f71748dd8a149b1
uufukttas <uufukttas@hotmail.com>
2024-03-11 09:41:13 +0300

    Update service point info data structure

--- apps/head-office/app/redux/features/updatedServicePointInfoData.ts
+++ apps/head-office/app/redux/features/updatedServicePointInfoData.ts
@@ -41,18 +41,18 @@ export const updatedServicePointInfoData = createSlice({
     initialState,
     reducers: {
         setUpdatedServicePointInfoData: (state, action) => {
-            state.updatedServicePointInfoData.address = action.payload.Address;
-            state.updatedServicePointInfoData.cityId = action.payload.CityID;
-            state.updatedServicePointInfoData.districtId = action.payload.DistrictID;
+            state.updatedServicePointInfoData.address = action.payload.address;
+            state.updatedServicePointInfoData.cityId = action.payload.cityID;
+            state.updatedServicePointInfoData.districtId = action.payload.districtID;
             state.updatedServicePointInfoData.freePark = action.payload.freePark;
-            state.updatedServicePointInfoData.id = action.payload.ID;
-            state.updatedServicePointInfoData.latitude = action.payload.Lat;
-            state.updatedServicePointInfoData.longitude = action.payload.Lon;
+            state.updatedServicePointInfoData.id = action.payload.id;
+            state.updatedServicePointInfoData.latitude = action.payload.lat;
+            state.updatedServicePointInfoData.longitude = action.payload.lon;
             state.updatedServicePointInfoData.name = action.payload.name;
             state.updatedServicePointInfoData.opportunities = action.payload.opportunities;
             state.updatedServicePointInfoData.paymentMethods = action.payload.paymentMethods;
-            state.updatedServicePointInfoData.phone1 = action.payload.Phone1;
-            state.updatedServicePointInfoData.phone2 = action.payload.Phone2;
+            state.updatedServicePointInfoData.phone1 = action.payload.phone1;
+            state.updatedServicePointInfoData.phone2 = action.payload.phone2;
             state.updatedServicePointInfoData.type = action.payload.type;
         },
     },
