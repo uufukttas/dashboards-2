ff23bb7ef290ed0188e1674b7aeb2c6bf3280fce
uufukttas <uufukttas@hotmail.com>
2024-03-13 16:50:29 +0300

    Refactor Loading component styling -rfc

--- apps/head-office/src/components/Loading/Loading.tsx
+++ apps/head-office/src/components/Loading/Loading.tsx
@@ -3,7 +3,9 @@ import './Loading.css';
 
 const Loading = () => {
   return (
-    <div className={`${BRAND_PREFIX}-loading-spinner-container flex items-center justify-center absolute top-0 left-0 w-full h-screen opacity-70 bg-[#54565A]`}>
+    <div
+      className={`${BRAND_PREFIX}-loading-spinner-container flex items-center justify-center absolute top-0 left-0 w-full h-screen opacity-70 bg-[#54565A]`}
+    >
       <div className="loading-spinner"></div>
     </div>
   );
