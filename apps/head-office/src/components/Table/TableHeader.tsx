8643d69f56a6babe4f7d608ba732980d726a792b
uufukttas <uufukttas@hotmail.com>
2024-03-19 13:32:39 +0300

    Fix height calculation in Section component and update search input styling in TableHeader component

--- apps/head-office/src/components/Table/TableHeader.tsx
+++ apps/head-office/src/components/Table/TableHeader.tsx
@@ -19,19 +19,19 @@ const TableActions = () => {
     return (
         <div className={`${BRAND_PREFIX}-table-actions-container flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 w-full md:flex-row border-t border-gray-300 bg-transparent`}>
             <Label className="sr-only" htmlFor="table-search" labelText={`Search`} />
-            <div className={`${BRAND_PREFIX}-service-point-search-input-container relative w-4/5 md:w-1/6 mx-2`}>
+            <div className={`${BRAND_PREFIX}-service-point-search-input-container relative w-full mx-2 md:w-1/2 lg:w-1/4`}>
                 <div className={`${BRAND_PREFIX}-service-point-search-icon-container absolute inset-y-0 flex items-center pl-5 pointer-events-none justify-end `}> 
                     <FaMagnifyingGlass  />
                 </div>
                 <Input
-                    className={`${BRAND_PREFIX}-service-point-search-input w-full block p-2 md:mx-2 pl-10 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500`}
+                    className={`${BRAND_PREFIX}-service-point-search-input w-full block p-2 md:mx-2 pl-10 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-yellow-500 focus:border-yellow-300`}
                     id="table-search-input"
                     name="search"
-                    placeholder="Search users"
+                    placeholder="Search"
                     type="text"
                 />
             </div>
-            <div className={`${BRAND_PREFIX}-table-actions w-4/5 md:w-1/6 mx-4`}>
+            <div className={`${BRAND_PREFIX}-table-actions w-full mx-2 md:w-1/2 lg:w-1/4 lg:mx-4`}>
                 <Button
                     className={`${BRAND_PREFIX}-add-service-point-button w-full bg-primary rounded-md font-semibold hover:bg-primary-lighter`}
                     type="button"
