83f72ecaee86dd8a129b975c010cd29864c2065e
uufukttas <uufukttas@hotmail.com>
2024-03-20 11:18:58 +0300

    Refactor ServicePointModal.tsx to remove unused useEffect hook

--- apps/head-office/src/components/ServicePointSection/ServicePointModal.tsx
+++ apps/head-office/src/components/ServicePointSection/ServicePointModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import ServicePointModalFormFirstPage from './ServicePointModalFormPages/ServicePointModalFormFirstPage';
 import ServicePointModalFormFourthPage from './ServicePointModalFormPages/ServicePointModalFormFourthPage';
