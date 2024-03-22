c96e36f1688690d750132ee93aa25c821f7b6ae9
uufukttas <uufukttas@hotmail.com>
2024-03-22 11:01:47 +0300

    Refactor Alert component to remove unused code and update styling

--- libs/alert/src/lib/alert.tsx
+++ libs/alert/src/lib/alert.tsx
@@ -1,44 +1,30 @@
-import { FaCircleInfo, FaRegCircleXmark } from "react-icons/fa6";
-import { Button } from '@projects/button';
+import { FaCircleInfo } from "react-icons/fa6";
 
-export interface IAlertProps {
+interface IAlertProps {
   alertText: string;
-  alertType: 'success' | 'error' | 'warning' | 'info';
+  alertType: 'error' | 'info' | 'success' | 'warning';
   id: string;
-  onClick?: () => void;
 };
 
-export function Alert({
-  alertText,
-  alertType,
-  id,
-  onClick,
-}: IAlertProps) {
-
+export function Alert({ alertText, alertType, id }: IAlertProps) {
   const alertTypes = {
-    success: 'bg-green-50 border-green-300 text-green-800',
     error: 'bg-red-50 border-red-300 text-red-800',
-    warning: 'bg-yellow-50 border-yellow-300 text-yellow-800',
     info: 'bg-blue-50 border-blue-300 text-blue-800',
+    success: 'bg-green-50 border-green-300 text-green-800',
+    warning: 'bg-yellow-50 border-yellow-300 text-yellow-800',
   };
 
   return (
     <div
       className={`alert-container flex items-center p-4 mb-4 border-t-4 absolute top-5 right-0 z-50 ${alertTypes[alertType]}`}
       id={id}
-      role="alert">
+    >
       <FaCircleInfo />
       <div className={`alert-text-container ms-3 text-sm font-medium px-5`}>
-        {alertText}
+        <span className={`alert-text`}>
+          {alertText}
+        </span>
       </div>
-      <Button
-        className={`alert-action-container ms-auto -mx-1.5 -my-1.5 rounded-lg p-1.5 inline-flex items-center justify-center h-8 w-8 hover:cursor-pointer`}
-        type='button'
-        onClick={onClick}
-      >
-        <span className="sr-only">Dismiss</span>
-        <FaRegCircleXmark  />
-      </Button>
     </div>
   );
 };
