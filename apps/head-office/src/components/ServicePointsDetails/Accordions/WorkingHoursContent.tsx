d38036d02e6110127b3d44e67c706230a17897ed
uufukttas <uufukttas@hotmail.com>
2024-03-26 16:04:10 +0300

    Refactor working hours functionality and add new features -norfc

--- apps/head-office/src/components/ServicePointsDetails/Accordions/WorkingHoursContent.tsx
+++ apps/head-office/src/components/ServicePointsDetails/Accordions/WorkingHoursContent.tsx
@@ -40,8 +40,42 @@ const WorkingHoursContent = ({ slug }: IWorkingHoursContentProps) => {
         { day: 5, hours: Array(24).fill(false) },
         { day: 6, hours: Array(24).fill(false) }
     ]);
-    const [selectedHours, setSelectedHours] = useState<{ day: number, hour: number }[]>([]);
     const days = ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar'];
+    const [isUpdated, setIsUpdated] = useState(false);
+    const [isPassive, setIsPassive] = useState(false); // isPassive state'ini ekledik
+
+
+    const handleChange = (dayIndex: number, hourIndex: number) => {
+        const newSchedule = [...schedule];
+        newSchedule[dayIndex].hours[hourIndex] = true;
+        setSchedule(newSchedule);
+
+    };
+
+    const togglePassive = () => {
+        setIsPassive(!isPassive);
+    }
+
+    const getWorkingHours = async () => {
+        try {
+
+            axios
+                .post(
+                    'https://sharztestapi.azurewebsites.net/ServicePoint/GetWorkHours',
+                    { stationID: 65824 },
+                    { headers: { 'Content-Type': 'application/json' } }
+                )
+                .then((response) => {
+                    console.log('response', response.data)
+                })
+                .catch((error) => { console.log('error', error) }
+                )
+
+        }
+        catch (error) {
+            console.log('error', error)
+        }
+    };
 
     const scheduleToHours = () => {
         const hours: { day: number, hour: number }[] = [];
@@ -84,45 +118,38 @@ const WorkingHoursContent = ({ slug }: IWorkingHoursContentProps) => {
         return result;
     };
 
-    const handleChange = (dayIndex: number, hourIndex: number) => {
-        const newSchedule = [...schedule];
-        newSchedule[dayIndex].hours[hourIndex] = !newSchedule[dayIndex].hours[hourIndex];
-        setSchedule(newSchedule);
-        setSelectedHours(scheduleToHours());
+    // const handleChange = (dayIndex: number, hourIndex: number) => {
+    //     const newSchedule = [...schedule];
+    //     newSchedule[dayIndex].hours[hourIndex] = !newSchedule[dayIndex].hours[hourIndex];
+    //     setSchedule(newSchedule);
+    //     setSelectedHours(scheduleToHours());
+    // };
+
+    // const setWorkingHours = () => {
+    //     axios
+    //         .post(
+    //             'https://sharztestapi.azurewebsites.net/ServicePoint/AddWorkHours',
+    //             JSON.stringify(convertResponseToStructure(selectedHours)),
+    //             { headers: { 'Content-Type': 'application/json' } }
+    //         )
+    //         .then((response) => console.log('response', response));
+    // };
+
+    // const setDefaultHours = (response: IResponse[]) => {
+    //     response.forEach(res => {
+    //         const index = schedule.findIndex(item => item.day === res.DayOfTheWeek);
+    //         if (index !== -1) {
+    //             const newSchedule = [...schedule];
+    //             newSchedule[index].hours[parseInt(res.OpeningTime)] = true;
+    //             setSchedule(newSchedule);
+    //         }
+    //     });
+    // };
+
+    const addWorkingHours = async () => {
     };
 
-    const setWorkingHours = () => {
-        axios
-            .post(
-                'https://sharztestapi.azurewebsites.net/ServicePoint/AddWorkHours',
-                JSON.stringify(convertResponseToStructure(selectedHours)),
-                { headers: { 'Content-Type': 'application/json' } }
-            )
-            .then((response) => console.log('response', response));
-    };
-
-    const setDefaultHours = (response: IResponse[]) => {
-        response.forEach(res => {
-            const index = schedule.findIndex(item => item.day === res.DayOfTheWeek);
-            if (index !== -1) {
-                const newSchedule = [...schedule]; // Yeni bir kopya oluştur
-                newSchedule[index].hours[parseInt(res.OpeningTime)] = true; // Saat 23:00'e karşılık gelen index 23 olduğu için true değeri atanıyor
-                setSchedule(newSchedule); // Yeni schedule'ı ayarla
-            }
-        });
-    };
-
-    const getWorkingHours = async () => {
-        await axios
-            .post(
-                'https://sharztestapi.azurewebsites.net/ServicePoint/GetWorkHours',
-                JSON.stringify({ stationID: slug }),
-                { headers: { 'Content-Type': 'application/json' } }
-            )
-            .then((response) => {
-                setDefaultHours(response.data);
-            });
-
+    const updateWorkingHours = async () => {
     };
 
     useEffect(() => {
@@ -149,7 +176,7 @@ const WorkingHoursContent = ({ slug }: IWorkingHoursContentProps) => {
                             <tr key={hourIndex}>
                                 <td className='w-[25px]'>{hourIndex}:00</td>
                                 {schedule.map((day, dayIndex) => (
-                                    <td key={dayIndex} className={`h-[20px] focus:ring-transparent ${day.hours[hourIndex] ? 'isPassive' : ''}`}>
+                                    <td key={dayIndex} className={`h-[20px] focus:ring-transparent ${isPassive ? 'isPassive' : ''}`} onClick={togglePassive}>
                                         <input
                                             checked={schedule[dayIndex].hours[hourIndex]}
                                             type="checkbox"
@@ -169,10 +196,10 @@ const WorkingHoursContent = ({ slug }: IWorkingHoursContentProps) => {
                 </table>
                 <div className='flex justify-end mt-4'>
                     <Button
-                        buttonText='Kaydet'
+                        buttonText={isUpdated ? 'Güncelle' : 'Ekle'}
                         className='bg-primary text-white rounded-md px-4 py-2'
                         type='button'
-                        onClick={setWorkingHours}
+                        onClick={isUpdated ? updateWorkingHours : addWorkingHours}
                     />
                 </div>
             </div>
