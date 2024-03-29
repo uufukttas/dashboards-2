415fa35f1f02a4a13fb4d7a915615d524fd37b24
uufukttas <uufukttas@hotmail.com>
2024-03-29 14:55:58 +0300

    Refactor ServicePointDetailsHeader component -rfc

--- apps/head-office/src/components/ServicePointsDetails/ServicePointDetailsHeader.tsx
+++ apps/head-office/src/components/ServicePointsDetails/ServicePointDetailsHeader.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import Card from '../Card/Card'
+import React from 'react';
+import Card from '../Card/Card';
+import { BRAND_PREFIX } from '../../constants/constants';
 
 interface IServicePointDetailsHeaderProps {
     servicePointDetailsName: string;
@@ -10,14 +11,14 @@ const ServicePointDetailsHeader = ({
     servicePointDetailsName, servicePointDetailsStatus
 }: IServicePointDetailsHeaderProps) => {
     const servicePointHeader = (
-        <div className="sh-service-point-details-page-header w-full flex items-center justify-between">
-            <div className="sh-service-point-details-page-header-left flex">
-                <h1 className="sh-service-point-details-page-title text-2xl font-bold text-heading">
+        <div className={`${BRAND_PREFIX}-service-point-details-page-header w-full flex items-center justify-between`}>
+            <div className={`${BRAND_PREFIX}-service-point-details-page-header-left flex`}>
+                <h1 className={`${BRAND_PREFIX}-service-point-details-page-title text-2xl font-bold text-heading`}>
                     {servicePointDetailsName}
                 </h1>
             </div>
-            <div className="sh-service-point-details-page-header-right flex">
-                <div className='sh-service-point-details-page-status text-sm font-bold'>
+            <div className={`${BRAND_PREFIX}-service-point-details-page-header-right flex`}>
+                <div className={`${BRAND_PREFIX}-service-point-details-page-status text-sm font-bold`}>
                     {
                         servicePointDetailsStatus
                             ? <div className='bg-green-500 rounded-full h-4 w-4 mx-2'></div>
