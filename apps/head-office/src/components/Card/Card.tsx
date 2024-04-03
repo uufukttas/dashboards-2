1aeeeee527d35fd762b60a16c0f4628d21f4324d
uufukttas <uufukttas@hotmail.com>
2024-04-03 10:43:18 +0300

    Refactor Card component structure -rfc

--- apps/head-office/src/components/Card/Card.tsx
+++ apps/head-office/src/components/Card/Card.tsx
@@ -1,24 +1,21 @@
-'use client';
-
 import CardBody from './CardBody';
 import CardFooter from './CardFooter';
 import CardHeader from './CardHeader';
 import { BRAND_PREFIX } from '../../constants/constants';
 import './Card.css';
 
+interface ICardContentProps {
+  body: React.JSX.Element;
+  header?: React.JSX.Element;
+  footer?: React.JSX.Element;
+};
+
 interface ICardProps {
-  cardContent: {
-    header?: React.JSX.Element;
-    body: React.JSX.Element;
-    footer?: React.JSX.Element;
-  };
+  cardContent: ICardContentProps;
   className?: string;
 };
 
-export function Card({
-  cardContent,
-  className,
-}: ICardProps) {
+export function Card({ cardContent, className }: ICardProps) {
   return (
     <div className={`${BRAND_PREFIX}-card-container p-8 shadow-custom lg:mx-8 ${className}`}>
       <CardHeader>
--- apps/head-office/src/components/Card/CardFooter.tsx
+++ apps/head-office/src/components/Card/CardFooter.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { BRAND_PREFIX } from '../../constants/constants';
 
 interface ICardFooterProps {
