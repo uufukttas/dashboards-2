1255aafab605a0d4728407cdb779c146d16554e3
uufukttas <uufukttas@hotmail.com>
2024-04-05 15:10:00 +0300

    Refactor PaginationItem component and update styles -rfc

--- apps/head-office/src/components/ServicePointSection/PaginationComponents/Pagination.tsx
+++ apps/head-office/src/components/ServicePointSection/PaginationComponents/Pagination.tsx
@@ -1,33 +1,27 @@
 import React from 'react';
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import PaginationItem from './PaginationItem';
-
-interface IPaginationProps {
-    currentPage: number;
-    totalPages: number;
-    setCurrentPage: (currentPage: number) => void;
-};
+import type { IPaginationProps } from '../types';
 
 const Pagination = ({ currentPage, totalPages, setCurrentPage }: IPaginationProps) => {
     const renderPageNumbers = () => {
-        const pageNumbers = [];
         const maxVisiblePages = 3;
+        const pageNumbers = [];
         let startPage = 1;
 
         if (currentPage > Math.floor(maxVisiblePages / 2)) {
             startPage = Math.max(currentPage - Math.floor(maxVisiblePages / 2), 1);
-        }
+        };
 
         const endPage = Math.min(startPage + maxVisiblePages - 1, totalPages);
 
         if (startPage > 1) {
             pageNumbers.push(
                 <PaginationItem
+                    isDisabled={false}
                     key={1}
-                    currentPage={currentPage}
                     page={1}
                     onClick={() => setCurrentPage(1)} isActive={false}
-                    isDisabled={false}
                 >
                     1
                 </PaginationItem>
@@ -41,48 +35,47 @@ const Pagination = ({ currentPage, totalPages, setCurrentPage }: IPaginationProp
                         ...
                     </li>
                 );
-            }
-        }
+            };
+        };
 
-        for (let i = startPage; i <= endPage; i++) {
+        for (let pageIndex = startPage; pageIndex <= endPage; pageIndex++) {
             pageNumbers.push(
                 <PaginationItem
-                    key={i}
-                    currentPage={currentPage}
-                    page={i}
-                    onClick={() => setCurrentPage(i)}
-                    isActive={i === currentPage}
+                    isActive={pageIndex === currentPage}
                     isDisabled={false}
+                    key={pageIndex}
+                    page={pageIndex}
+                    onClick={() => setCurrentPage(pageIndex)}
                 >
-                    {i}
+                    {pageIndex}
                 </PaginationItem>
             );
-        }
+        };
 
         if (endPage < totalPages) {
             if (endPage < totalPages - 1) {
                 pageNumbers.push(
                     <li
-                        key="ellipsisEnd"
                         className='flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg'
+                        key="ellipsisEnd"
                     >
                         ...
                     </li>
                 );
-            }
+            };
+
             pageNumbers.push(
                 <PaginationItem
-                    key={totalPages}
-                    currentPage={currentPage}
-                    page={totalPages}
-                    onClick={() => setCurrentPage(totalPages)}
                     isActive={false}
                     isDisabled={false}
+                    key={totalPages}
+                    page={totalPages}
+                    onClick={() => setCurrentPage(totalPages)}
                 >
                     {totalPages}
                 </PaginationItem>
             );
-        }
+        };
 
         return pageNumbers;
     };
@@ -91,21 +84,19 @@ const Pagination = ({ currentPage, totalPages, setCurrentPage }: IPaginationProp
         <nav aria-label="Page navigation">
             <ul className="pagination flex items-center -space-x-px h-8 text-sm mt-4">
                 <PaginationItem
-                    currentPage={currentPage}
-                    page={currentPage - 1}
-                    onClick={() => setCurrentPage(currentPage - 1)}
                     isActive={false}
                     isDisabled={currentPage === 1}
+                    page={currentPage - 1}
+                    onClick={() => setCurrentPage(currentPage - 1)}
                 >
                     <FaChevronLeft />
                 </PaginationItem>
                 {renderPageNumbers()}
                 <PaginationItem
-                    currentPage={currentPage}
-                    page={currentPage + 1}
-                    onClick={() => setCurrentPage(currentPage + 1)}
                     isActive={false}
                     isDisabled={currentPage === totalPages}
+                    page={currentPage + 1}
+                    onClick={() => setCurrentPage(currentPage + 1)}
                 >
                     <FaChevronRight />
                 </PaginationItem>
---
+++ apps/head-office/src/components/ServicePointSection/PaginationComponents/PaginationItem.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button } from '@projects/button';
+import type { IPaginationItemProps } from '../types';
+
+const PaginationItem = ({ page, onClick, children, isActive, isDisabled }: IPaginationItemProps) => {
+    const className =
+        `flex items-center justify-center px-3 h-8 leading-tight border ${
+            isActive
+                ? 'z-10 text-yellow-600 bg-yellow-50 border-yellow-300 hover:bg-yellow-100 hover:text-yellow-700'
+                : 'text-gray-500 bg-white border-gray-300 hover:bg-gray-100 hover:text-gray-700'
+        } ${
+            isDisabled
+                ? 'hidden'
+                : 'flex'
+        }`;
+
+    return (
+        <li className={isActive ? 'active' : ''}>
+            <Button
+                aria-current={isActive ? "page" : undefined}
+                className={className}
+                id={`pagination-item-${page}`}
+                type='button'
+                onClick={onClick}
+            >
+                {children}
+            </Button>
+        </li>
+    );
+};
+
+export default PaginationItem;
