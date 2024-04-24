c50240fb59960597e6675a757e78c38632c61b4c
uufukttas <uufukttas@hotmail.com>
2024-04-24 12:10:51 +0300

    Update Background and Loading components -rfc

--- apps/head-office/src/components/Background/Background.tsx
+++ apps/head-office/src/components/Background/Background.tsx
@@ -2,7 +2,7 @@ import { BRAND_PREFIX } from '../../constants/constants';
 import type { IBackgroundProps } from './types';
 import './Background.css';
 
-const Background = ({ backgroundUrl, className }: IBackgroundProps) => {
+const Background = ({ backgroundUrl, className }: IBackgroundProps): React.ReactNode => {
   return (
     <div
       className={`${BRAND_PREFIX}-background-container h-screen ${className}`}
--- apps/head-office/src/components/Loading/Loading.tsx
+++ apps/head-office/src/components/Loading/Loading.tsx
@@ -1,9 +1,11 @@
-import { BRAND_PREFIX } from '../../constants/constants';
+import { BRAND_PREFIX, stylesProps } from '../../constants/constants';
 import './Loading.css';
 
-const Loading = () => {
+const Loading: React.FC = () => {
   return (
-    <div className={`${BRAND_PREFIX}-loading-spinner-container flex items-center justify-center absolute top-0 left-0 w-full h-screen opacity-70 bg-[#54565A]`}>
+    <div
+      className={`${BRAND_PREFIX}-loading-spinner-container flex items-center justify-center absolute top-0 left-0 w-full h-screen opacity-70 bg-[${stylesProps.loginFormBackgroundColor}]`}
+    >
       <div className="loading-spinner"></div>
     </div>
   );
