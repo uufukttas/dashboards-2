e2eb4e864b2f59e998dfc6c1d168a99af30b625b
uufukttas <uufukttas@hotmail.com>
2024-04-24 09:36:16 +0300

    Update TableHead component to center align column headers in TableComponents folder

--- apps/head-office/src/components/Table/TableComponents/TableHead.tsx
+++ apps/head-office/src/components/Table/TableComponents/TableHead.tsx
@@ -5,22 +5,25 @@ const TableHead = () => {
     return (
         <thead className={`${BRAND_PREFIX}-service-points-table-head text-xs text-gray-700 uppercase bg-gray-50`}>
             <tr>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-6 py-3 text-center">
                     Servis Noktasi
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-6 py-3 text-center">
                     Telefon No
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-6 py-3 text-center">
                     Adres
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-6 py-3 text-center">
                     Il
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-6 py-3 text-center">
                     Ilce
                 </th>
-                <th scope="col" className="px-6 py-3 flex justify-center">
+                <th scope="col" className="px-6 py-3 text-center">
+                    Aktif
+                </th>
+                <th scope="col" className="px-6 py-3 text-center">
                     Aksiyonlar
                 </th>
             </tr>
