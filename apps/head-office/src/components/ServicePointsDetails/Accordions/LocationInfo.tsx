df72373eb8a9d9e39f5f07ac166d70623897052c
uufukttas <uufukttas@hotmail.com>
2024-04-26 10:49:50 +0300

    Refactor working hours styles and class names in ServicePointsDetails component -rfc

--- apps/head-office/src/components/ServicePointsDetails/Accordions/LocationInfo.tsx
+++ apps/head-office/src/components/ServicePointsDetails/Accordions/LocationInfo.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { CITIES, DISTRICTS } from '../../../constants/constants';
 import { toggleLoadingVisibility } from '../../../../app/redux/features/isLoadingVisible';
-import type { IServiceDetailsContentProps, IServicePointsDetailsInfoProps } from '../types';
+import type { IServiceDetailsContentProps, IServicePointsDetailsInfoProps, IServicePointsDetailsProps } from '../types';
 
 const ServicePointDetailsContent: React.FC<IServiceDetailsContentProps> = ({ slug }: IServiceDetailsContentProps) => {
     const initialServicePointsDetailsInfoStateValue = {
@@ -17,13 +17,23 @@ const ServicePointDetailsContent: React.FC<IServiceDetailsContentProps> = ({ slu
         phone2: '',
         stationId: 0,
     };
+    const initialServicePointDataValue = {
+        id: 0,
+        name: '',
+        companyId: 1,
+        companyName: '',
+        resellerCompanyId: 0,
+        resellerName: '',
+        isActive: true,
+        isDeleted: false
+    }
     const sectionPrefix = 'service-point-details';
     const dispatch = useDispatch();
     const [servicePointDetailsInfo, setServicePointDetailsInfo] =
         useState<IServicePointsDetailsInfoProps>(
             initialServicePointsDetailsInfoStateValue
         );
-
+    const [servicePointData, setServicePointData] = useState<IServicePointsDetailsProps>(initialServicePointDataValue);
     const getSelectedCity = (cityId: number) => CITIES[cityId?.toString()];
     const getSelectedDistrict = (districtId: number) => DISTRICTS[districtId?.toString()];
     const getServicePointsDetailsInfo = async (slug: string) => {
@@ -40,65 +50,122 @@ const ServicePointDetailsContent: React.FC<IServiceDetailsContentProps> = ({ slu
             })
             .catch((error) => console.log(error));
     };
+    const getServicePointData = async (slug: string) => {
+        axios
+        .post(
+            process.env.GET_STATION_BY_ID || '',
+            JSON.stringify({ id: Number(slug) }),
+            { headers: { 'Content-Type': 'application/json' } }
+        )
+        .then((response) => response.data)
+        .then((data) => {
+            data.data && setServicePointData(data.data[0]);
+        })
+    };
 
     useEffect(() => {
         getServicePointsDetailsInfo(slug);
+        getServicePointData(slug);
     }, []);
 
     return (
         <div className={`${sectionPrefix}-content py-8 text-text`}>
-            <div className={`${sectionPrefix}-info-container`}>
-                <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
-                    <p className={`${sectionPrefix}-info-item-label text-lg font-bold md:w-1/12`}>
-                        Adres:
-                    </p>
-                    <p className={`${sectionPrefix}-info-item-value text-lg font-normal`}>
-                        {servicePointDetailsInfo.address}
-                    </p>
+            <div className={`${sectionPrefix}-info-container flex justify-between`}>
+                <div className={`${sectionPrefix}-left-info-container flex flex-col w-1/2`}>
+                    <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
+                        <p className={`${sectionPrefix}-info-item-label text-lg font-bold`}>
+                            Adres:
+                        </p>
+                        <p className={`${sectionPrefix}-info-item-value text-lg font-normal px-2`}>
+                            {servicePointDetailsInfo.address}
+                        </p>
+                    </div>
+                    <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
+                        <p className={`${sectionPrefix}-info-item-label text-lg font-bold `}>
+                            Telefon:
+                        </p>
+                        <p className={`${sectionPrefix}-info-item-value text-lg font-normal px-2`}>
+                            {servicePointDetailsInfo.phone1}
+                        </p>
+                    </div>
+                    {
+                        servicePointDetailsInfo.phone2 && (
+                            <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
+                                <p className={`${sectionPrefix}-info-item-label text-lg font-bold `}>
+                                    Telefon 2:
+                                </p>
+                                <p className={`${sectionPrefix}-info-item-value text-lg px-2`}>
+                                    {servicePointDetailsInfo.phone2}
+                                </p>
+                            </div>
+                        )
+                    }
+                    <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
+                        <p className={`${sectionPrefix}-info-item-label text-lg font-bold `}>
+                            Il:
+                        </p>
+                        <p className={`${sectionPrefix}-info-item-value text-lg font-normal px-2`}>
+                            {getSelectedCity(servicePointDetailsInfo.cityId)}
+                        </p>
+                    </div>
+                    <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
+                        <p className={`${sectionPrefix}-info-item-label text-lg font-bold `}>
+                            Ilce:
+                        </p>
+                        <p className={`${sectionPrefix}-info-item-value text-lg font-normal px-2`}>
+                            {getSelectedDistrict(servicePointDetailsInfo.districtId)}
+                        </p>
+                    </div>
+                    <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
+                        <p className={`${sectionPrefix}-info-item-label text-lg font-bold `}>
+                            Konum:
+                        </p>
+                        <p className={`${sectionPrefix}-info-item-value text-lg font-normal px-2`}>
+                            {servicePointDetailsInfo.lat} - {servicePointDetailsInfo.lon}
+                        </p>
+                    </div>
                 </div>
-                <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
-                    <p className={`${sectionPrefix}-info-item-label text-lg font-bold w-1/12`}>
-                        Telefon:
-                    </p>
-                    <p className={`${sectionPrefix}-info-item-value text-lg font-normal`}>
-                        {servicePointDetailsInfo.phone1}
-                    </p>
-                </div>
-                {
-                    servicePointDetailsInfo.phone2 && (
-                        <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
-                            <p className={`${sectionPrefix}-info-item-label text-lg font-bold w-1/12`}>
-                                Telefon 2:
-                            </p>
-                            <p className={`${sectionPrefix}-info-item-value text-lg`}>
-                                {servicePointDetailsInfo.phone2}
-                            </p>
-                        </div>
-                    )
-                }
-                <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
-                    <p className={`${sectionPrefix}-info-item-label text-lg font-bold w-1/12`}>
-                        Il:
-                    </p>
-                    <p className={`${sectionPrefix}-info-item-value text-lg font-normal`}>
-                        {getSelectedCity(servicePointDetailsInfo.cityId)}
-                    </p>
-                </div>
-                <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
-                    <p className={`${sectionPrefix}-info-item-label text-lg font-bold w-1/12`}>
-                        Ilce:
-                    </p>
-                    <p className={`${sectionPrefix}-info-item-value text-lg font-normal`}>
-                        {getSelectedDistrict(servicePointDetailsInfo.districtId)}
-                    </p>
-                </div>
-                <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
-                    <p className={`${sectionPrefix}-info-item-label text-lg font-bold w-1/12`}>
-                        Konum:
-                    </p>
-                    <p className={`${sectionPrefix}-info-item-value text-lg font-normal`}>
-                        {servicePointDetailsInfo.lat} - {servicePointDetailsInfo.lon}
-                    </p>
+                <div className={`${sectionPrefix}-right-info-container flex flex-col w-1/2`}>
+                    <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
+                        <p className={`${sectionPrefix}-info-item-label text-lg font-bold`}>
+                            Sirket:
+                        </p>
+                        <p className={`${sectionPrefix}-info-item-value text-lg font-normal px-2`}>
+                            {servicePointData.companyName}
+                        </p>
+                    </div>
+                    <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
+                        <p className={`${sectionPrefix}-info-item-label text-lg font-bold`}>
+                            Bayi:
+                        </p>
+                        <p className={`${sectionPrefix}-info-item-value text-lg font-normal px-2`}>
+                            {servicePointData.resellerName}
+                        </p>
+                    </div>
+                    <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
+                        <p className={`${sectionPrefix}-info-item-label text-lg font-bold`}>
+                            Odeme Yontemleri:
+                        </p>
+                        <p className={`${sectionPrefix}-info-item-value text-lg font-normal px-2`}>
+                            {/* {servicePointDetailsInfo.resellerName} */}
+                        </p>
+                    </div>
+                    <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
+                        <p className={`${sectionPrefix}-info-item-label text-lg font-bold`}>
+                            Park Yeri:
+                        </p>
+                        <p className={`${sectionPrefix}-info-item-value text-lg font-normal px-2`}>
+                            {/* {servicePointDetailsInfo.resellerName} */}
+                        </p>
+                    </div>
+                    <div className={`${sectionPrefix}-info-item flex justify-start md:items-center flex-col md:flex-row`}>
+                        <p className={`${sectionPrefix}-info-item-label text-lg font-bold`}>
+                            Servis Noktasi Olanaklari:
+                        </p>
+                        <p className={`${sectionPrefix}-info-item-value text-lg font-normal px-2`}>
+                            {/* {servicePointDetailsInfo.resellerName} */}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
