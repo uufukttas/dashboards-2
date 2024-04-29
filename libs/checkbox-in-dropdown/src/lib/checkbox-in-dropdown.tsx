ca3febc18ac727bc57a35b99cb423e3238667da3
uufukttas <uufukttas@hotmail.com>
2024-04-29 10:54:19 +0300

    Refactor CheckboxInDropdown component to add inputName prop and update styles -rfc

--- libs/checkbox-in-dropdown/src/lib/checkbox-in-dropdown.tsx
+++ libs/checkbox-in-dropdown/src/lib/checkbox-in-dropdown.tsx
@@ -5,17 +5,19 @@ import { Label } from '@projects/label';
 
 interface IDropdownItemProps {
   name: string;
-  id: number;
+  id: number | null;
+  rid: number | null;
 };
 
 interface IDropdownProps {
   className: string;
   id: string;
+  inputName: string;
   items: IDropdownItemProps[];
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export function CheckboxInDropdown({ className, id, items, onChange }: IDropdownProps) {
+export function CheckboxInDropdown({ className, id, inputName, items, onChange }: IDropdownProps) {
   const dropdownRef = useRef<HTMLDivElement>(null);
   const [isOpen, setIsOpen] = useState(false);
 
@@ -36,16 +38,24 @@ export function CheckboxInDropdown({ className, id, items, onChange }: IDropdown
   }, []);
 
   return (
-    <div className={`checkbox-in-dropdown-wrapper relative`} id={id} ref={dropdownRef}>
-      <Button
-        className="px-4 py-2 rounded-md border w-full text-left"
-        id="checkbox-in-dropdown-button"
-        type='button'
-        onClick={toggleDropdown}>
-        Select Options
-      </Button>
+    <div className={`checkbox-in-dropdown-wrapper relative rounded-md border w-full text-left`} id={id} ref={dropdownRef}>
+      <div className='flex w-full'>
+        <Button
+          className="w-full px-4 py-2 text-left flex justify-between items-center"
+          id="checkbox-in-dropdown-button"
+          type='button'
+          onClick={toggleDropdown}>
+          Seciminizi Yapiniz
+          <div
+            className='arrow-icon text-lg text-gray-900'
+            dangerouslySetInnerHTML={{
+              __html: `${isOpen ? '&#11205;' : '&#11206;'}`,
+            }}
+          />
+        </Button>
+      </div>
       <div
-        className={`${isOpen ? 'absolute bg-white shadow-lg w-48 rounded-md z-10' : 'hidden'} checkbox-in-dropdown-input-wrapper border w-full border-t-0`}
+        className={`${isOpen ? 'absolute bg-white shadow-lg w-48 rounded-md z-10' : 'hidden'} checkbox-in-dropdown-input-wrapper border w-full`}
         id="checkbox-in-dropdown-input-wrapper"
       >
         {
@@ -54,10 +64,10 @@ export function CheckboxInDropdown({ className, id, items, onChange }: IDropdown
               <Label className="flex items-center space-x-2 checkbox-in-dropdown-label" htmlFor='' labelText=''>
                 <Input
                   className="form-checkbox h-5 w-5 checkbox-in-dropdown-input"
-                  id={''}
-                  name={''}
+                  id={`checkbox-in-dropdown-input-${item.id || item.rid}`}
+                  name={inputName}
                   type="checkbox"
-                  value={item.id}
+                  value={item.id || item.rid || 0}
                   onChange={onChange}
                 />
                 <span>{item.name}</span>
