4f183af5208feeba71fe606a2893d4ee86e7b67a
uufukttas <uufukttas@hotmail.com>
2024-05-02 16:24:15 +0300

    Refactor CardHeader component to remove unnecessary width class -rfc

--- apps/head-office/src/components/Card/CardHeader.tsx
+++ apps/head-office/src/components/Card/CardHeader.tsx
@@ -2,7 +2,7 @@ import { BRAND_PREFIX } from '../../constants/constants';
 
 const CardHeader = ({ children }: { children: React.ReactNode }): React.ReactNode => {
     return (
-        <div className={`${BRAND_PREFIX}-card-header-container flex items-center w-full justify-between`}>
+        <div className={`${BRAND_PREFIX}-card-header-container flex items-center justify-between`}>
             {children}
         </div>
     );
