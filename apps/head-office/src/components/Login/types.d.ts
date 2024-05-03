4df052779dc568034db8253c31f0fb74d0e782be
uufukttas <uufukttas@hotmail.com>
2024-05-03 15:31:34 +0300

    refactor: Update Login component types to match API response -rfc

--- apps/head-office/src/components/Login/types.d.ts
+++ apps/head-office/src/components/Login/types.d.ts
@@ -1,8 +1,19 @@
-interface IHeaderProps {
+interface IContentTypeProps {
     [key: string]: string;
 };
-
+interface IResponseStatusProps {
+    status: number;
+};
+export interface IHeaderProps {
+    headers: IContentTypeProps
+};
 export interface ILoginFormDataProps {
     username: string;
     password: string;
+};
+export interface ILoginRequestDataProps {
+    [key: string]: string;
+};
+export interface IResponseErrorProps {
+    response: IResponseStatusProps;
 };
