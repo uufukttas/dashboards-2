78990e20ba63fe9a5575b8456a9bf8a859bd1c6b
uufukttas <uufukttas@hotmail.com>
2024-05-06 16:02:43 +0300

    refactor: Update Loading component to use correct background color -rfc

--- apps/head-office/src/components/Loading/Loading.tsx
+++ apps/head-office/src/components/Loading/Loading.tsx
@@ -1,10 +1,10 @@
-import { BRAND_PREFIX, stylesProps } from '../../constants/constants';
+import { BRAND_PREFIX } from '../../constants/constants';
 import './Loading.css';
 
 const Loading: React.FC = () => {
   return (
     <div
-      className={`${BRAND_PREFIX}-loading-spinner-container flex items-center justify-center absolute top-0 left-0 w-full h-screen opacity-70 bg-[${stylesProps.loginFormBackgroundColor}]`}
+      className={`${BRAND_PREFIX}-loading-spinner-container flex items-center justify-center absolute top-0 left-0 w-full h-screen opacity-70 bg-[#54565A]`}
     >
       <div className="loading-spinner"></div>
     </div>
