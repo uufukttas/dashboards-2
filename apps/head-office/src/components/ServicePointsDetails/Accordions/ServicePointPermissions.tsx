559656fe8a485aa97f5f37d209319e5ee1ac5a8d
uufukttas <uufukttas@hotmail.com>
2024-05-08 13:44:32 +0300

    feat: Add ServicePointPermissions component to ServicePointsDetails page -rfc

---
+++ apps/head-office/src/components/ServicePointsDetails/Accordions/ServicePointPermissions.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServicePointPermissions = () => {
+    return (
+        <div className='flex flex-col items-end py-4 text-white'>
+            asd
+        </div>
+    );
+};
+
+export default ServicePointPermissions;
