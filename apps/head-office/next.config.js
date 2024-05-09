c8e77fa637fdb56672b506924f4afaaf58cf5a07
uufukttas <uufukttas@hotmail.com>
2024-05-09 09:03:09 +0300

    chore: Remove commented out code in next.config.js

--- apps/head-office/next.config.js
+++ apps/head-office/next.config.js
@@ -56,15 +56,6 @@ const nextConfig = {
       },
     ];
   },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/dashboards',
-  //       destination: '/dashboards.html',
-  //       permanent: true,  // Kalıcı yönlendirme (HTTP 301), false ise geçici (HTTP 307)
-  //     },
-  //   ];
-  // },
 };
 
 const plugins = [withNx];
