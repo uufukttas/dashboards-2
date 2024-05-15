aa07b19bac62caffba09e5afd421686f26f56e1d
uufukttas <uufukttas@hotmail.com>
2024-05-15 13:39:10 +0300

    refactor: Update Background component to use brand prefix variable

--- apps/head-office/src/components/Background/Background.tsx
+++ apps/head-office/src/components/Background/Background.tsx
@@ -3,9 +3,10 @@ import type { IBackgroundProps } from './types';
 import './Background.css';
 
 const Background: React.FC<IBackgroundProps> = ({ backgroundUrl, className }: IBackgroundProps) => {
+  const backgroundPrefix = `${BRAND_PREFIX}-background`;
   return (
     <div
-      className={`${BRAND_PREFIX}-background-container h-screen ${className}`}
+      className={`${backgroundPrefix}-container h-screen ${className}`}
       style={{ backgroundImage: `url('${backgroundUrl}')` }}
     >
     </div>
