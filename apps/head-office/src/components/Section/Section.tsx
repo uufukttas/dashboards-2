328ef999d6fe31597c24661ae7bf0b28a037111f
uufukttas <uufukttas@hotmail.com>
2024-05-15 14:46:05 +0300

    refactor: Update Section component to use brand prefix variable

--- apps/head-office/src/components/Section/Section.tsx
+++ apps/head-office/src/components/Section/Section.tsx
@@ -3,11 +3,13 @@ import type { ISectionProps } from './types';
 import './Section.css';
 
 const Section: React.FC<ISectionProps> = ({ children, sectionName }: ISectionProps) => {
+  const sectionPrefix = `${BRAND_PREFIX}-section`;
+
   return (
     <section
-      className={`${BRAND_PREFIX}-section-container flex-wrap w-full px-8 xl:px-20 py-4 bg-white py-4 h-auto min-h-full`}>
-      <div className={`${BRAND_PREFIX}-section-header-container`}>
-        <h2 className={`${BRAND_PREFIX}-section-header-text text-text text-lg font-bold`}>
+      className={`${sectionPrefix}-container flex-wrap w-full px-8 xl:px-20 py-4 bg-white py-4 h-auto min-h-full`}>
+      <div className={`${sectionPrefix}-header-container`}>
+        <h2 className={`${sectionPrefix}-header-text text-text text-lg font-bold`}>
           {sectionName}
         </h2>
       </div>
