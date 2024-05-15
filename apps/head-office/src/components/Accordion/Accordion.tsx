319654650743dbe2d3c5633d473977e732f5146a
uufukttas <uufukttas@hotmail.com>
2024-05-15 13:03:36 +0300

    refactor: Update Accordion component to use brand prefix variable

--- apps/head-office/src/components/Accordion/Accordion.tsx
+++ apps/head-office/src/components/Accordion/Accordion.tsx
@@ -1,17 +1,10 @@
 import React, { useState } from 'react';
 import { BRAND_PREFIX } from '../../constants/constants';
+import { IAccordionProps } from './types';
 import './Accordion.css';
 
-interface IAccordionProps {
-  accordionIcon?: React.ReactNode;
-  accordionTitle: string;
-  actionButton?: React.ReactNode;
-  children: React.ReactNode;
-  contentClassName?: string;
-  titleClassName?: string;
-};
-
 const Accordion = ({ accordionIcon, accordionTitle, actionButton, children, contentClassName = "", titleClassName }: IAccordionProps) => {
+  const accordionPrefix = 'accordion';
   const [isOpen, setIsOpen] = useState(true);
 
   const toggleAccordion = () => {
@@ -19,30 +12,35 @@ const Accordion = ({ accordionIcon, accordionTitle, actionButton, children, cont
   };
 
   return (
-    <div className={`${BRAND_PREFIX}-accordion-content-container border my-4 rounded-lg`}>
-      <div className={`${BRAND_PREFIX}-accordion-header justify-between bg-primary`}>
-        <div className={`${BRAND_PREFIX}-accordion-left-container flex items-center ${titleClassName}`}>
-          <div className={`${BRAND_PREFIX}-accordion-title px-2`}>
+    <div className={`${BRAND_PREFIX}-${accordionPrefix}-content-container border my-4 rounded-lg`}>
+      <div className={`${BRAND_PREFIX}-${accordionPrefix}-header justify-between bg-primary`}>
+        <div className={`${BRAND_PREFIX}-${accordionPrefix}-left-container flex items-center ${titleClassName}`}>
+          <div className={`${BRAND_PREFIX}-${accordionPrefix}-title px-2`}>
             {accordionTitle}
           </div>
           <div className={`${BRAND_PREFIX}-icon-container px-2`}>
             {accordionIcon}
           </div>
         </div>
-        <div className={`${BRAND_PREFIX}-accordion-right-container flex items-center justify-center`}>
+        <div className={`${BRAND_PREFIX}-${accordionPrefix}-right-container flex items-center justify-center`}>
           <div className={`${BRAND_PREFIX}-action-button-container`}>
             {actionButton}
           </div>
-          <div className={`${BRAND_PREFIX}-accordion-arrow ${isOpen ? 'open' : ''}`} onClick={toggleAccordion}></div>
+          <div
+            className={`${BRAND_PREFIX}-${accordionPrefix}-arrow ${isOpen ? 'open' : ''}`}
+            onClick={toggleAccordion}>
+          </div>
         </div>
       </div>
       {
-        <div className={`${BRAND_PREFIX}-accordion-content ${isOpen
-          ? 'open'
-          : 'close'
-          } px-4 md:px-8 bg-white rounded-lg ${contentClassName}`}
+        <div className={
+          `${BRAND_PREFIX}-${accordionPrefix}-content ${isOpen
+            ? 'open'
+            : 'close'
+          } px-4 md:px-8 bg-white rounded-lg ${contentClassName}`
+        }
         >
-          {children && children}
+          {children}
         </div>
       }
     </div>
---
+++ apps/head-office/src/components/Accordion/types.d.ts
@@ -0,0 +1,8 @@
+export interface IAccordionProps {
+    accordionIcon?: React.ReactNode;
+    accordionTitle: string;
+    actionButton?: React.ReactNode;
+    children: React.ReactNode;
+    contentClassName?: string;
+    titleClassName?: string;
+};
