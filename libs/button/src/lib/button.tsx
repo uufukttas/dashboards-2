e2818079540588435bd1f961948785c2d6972dab
uufukttas <uufukttas@hotmail.com>
2024-05-17 10:33:46 +0300

    refactor: Add disabled prop to Button component

--- libs/button/src/lib/button.tsx
+++ libs/button/src/lib/button.tsx
@@ -3,15 +3,17 @@ interface IButtonProps {
   children?: React.ReactNode;
   className?: string;
   dataAttributes?: { [key: string]: string };
+  disabled?: boolean;
   id: string;
   type: 'button' | 'submit' | 'reset';
   onClick?: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
 };
 
-export function Button({ buttonText, children, className, dataAttributes, id, type, onClick }: IButtonProps) {
+export function Button({ buttonText, children, className, dataAttributes, disabled, id, type, onClick }: IButtonProps) {
   return (
     <button
       className={`button ${className}`}
+      disabled={disabled}
       id={id}
       type={type}
       onClick={onClick}
