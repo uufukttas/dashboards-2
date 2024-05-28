64cac9f828b944b42612e433a5aee688f2e3f47e
uufukttas <uufukttas@hotmail.com>
2024-05-28 16:38:59 +0300

    (feat): added searchbox to the maps

--- apps/head-office/src/components/ServicePointSection/Map.tsx
+++ apps/head-office/src/components/ServicePointSection/Map.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import axios from 'axios';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, StandaloneSearchBox } from '@react-google-maps/api';
 import { CITIES, DISTRICTS } from '../../constants/constants';
 import { IFormDataProps } from './types';
 
@@ -17,6 +17,29 @@ interface MapProps {
 const MapComponent: React.FC<MapProps> = ({ formData, onSelectLocation }) => {
     const [markerPosition, setMarkerPosition] = useState<Location | null>(null);
     const [mapCenter, setMapCenter] = useState<Location>({ lat: 41.0848993, lng: 28.9765895 });
+    const libraries = ["places"];
+    const mapRef = useRef<google.maps.Map>(null);
+    const searchBoxRef = useRef<google.maps.places.SearchBox>(null);
+
+    const onLoad = useCallback(function callback(map: google.maps.Map) {
+        mapRef.current = map;
+    }, []);
+
+    const onSearchBoxLoad = useCallback(function callback(searchBox: google.maps.places.SearchBox) {
+        searchBoxRef.current = searchBox;
+    }, []);
+
+    const onPlacesChanged = () => {
+        const places = searchBoxRef.current?.getPlaces();
+        if (places && places.length > 0) {
+            const location = places[0].geometry?.location;
+
+            if (location) {
+                mapRef.current?.setCenter(location);
+                mapRef.current?.setZoom(16);
+            }
+        }
+    };
 
     const handleMapClick = (event: google.maps.MapMouseEvent) => {
         const lat = event.latLng?.lat();
@@ -49,27 +72,40 @@ const MapComponent: React.FC<MapProps> = ({ formData, onSelectLocation }) => {
     }, [formData[`service-point-cityId`], formData[`service-point-districtId`]]);
 
     return (
-        <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}>
-            <GoogleMap
-                mapContainerStyle={{ width: '584px', height: '300px' }}
-                center={mapCenter}
-                zoom={10}
-                onClick={handleMapClick}
-            >
-                {markerPosition && (
-                    <Marker
-                        position={markerPosition}
-                        draggable={true}
-                        onDragEnd={(event: google.maps.MapMouseEvent) => {
-                            const lat = event?.latLng?.lat() || 41.0848993;
-                            const lng = event?.latLng?.lng() || 28.9765895;
-
-                            onSelectLocation({ lat, lng });
-                        }}
+        <div className='map-container relative'>
+            <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''} libraries={libraries}>
+                <StandaloneSearchBox
+                    onLoad={onSearchBoxLoad}
+                    onPlacesChanged={onPlacesChanged}
+                >
+                    <input
+                        type="text"
+                        placeholder="Search places..."
+                        style={{ zIndex: 10, boxSizing: 'border-box', border: '1px solid transparent', width: '240px', height: '32px', padding: '0 12px', borderRadius: '3px', boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)', fontSize: '14px', outline: 'none', textOverflow: 'ellipses', position: 'absolute', top: '10px', left: '50%', marginLeft: '-120px' }}
                     />
-                )}
-            </GoogleMap>
-        </LoadScript>
+                </StandaloneSearchBox>
+                <GoogleMap
+                    mapContainerStyle={{ width: '584px', height: '300px' }}
+                    center={mapCenter}
+                    zoom={10}
+                    onClick={handleMapClick}
+                    onLoad={onLoad}
+                >
+                    {markerPosition && (
+                        <Marker
+                            position={markerPosition}
+                            draggable={true}
+                            onDragEnd={(event: google.maps.MapMouseEvent) => {
+                                const lat = event?.latLng?.lat() || 41.0848993;
+                                const lng = event?.latLng?.lng() || 28.9765895;
+
+                                onSelectLocation({ lat, lng });
+                            }}
+                        />
+                    )}
+                </GoogleMap>
+            </LoadScript>
+        </div>
     );
 }
 
