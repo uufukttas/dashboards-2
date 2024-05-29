e524803c2b0735a36d08ffaaeb1d966109de4885
uufukttas <uufukttas@hotmail.com>
2024-05-29 11:13:51 +0300

    refactor: Update MapComponent searchbox height to 40px

--- apps/head-office/src/components/ServicePointSection/Map.tsx
+++ apps/head-office/src/components/ServicePointSection/Map.tsx
@@ -84,7 +84,7 @@ const MapComponent: React.FC<MapProps> = ({ formData, onSelectLocation }) => {
                     <input
                         type="text"
                         placeholder="Search places..."
-                        style={{ zIndex: 10, boxSizing: 'border-box', border: '1px solid transparent', width: '240px', height: '32px', padding: '0 12px', borderRadius: '3px', boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)', fontSize: '14px', outline: 'none', textOverflow: 'ellipses', position: 'absolute', top: '10px', left: '50%', marginLeft: '-120px' }}
+                        style={{ zIndex: 10, boxSizing: 'border-box', border: '1px solid transparent', width: '240px', height: '40px', padding: '0 12px', borderRadius: '3px', boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)', fontSize: '14px', outline: 'none', textOverflow: 'ellipses', position: 'absolute', top: '10px', left: '50%', marginLeft: '-120px' }}
                     />
                 </StandaloneSearchBox>
                 <GoogleMap
