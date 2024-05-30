136a310ee4043205ce7c33216643736c8f0ab875
uufukttas <uufukttas@hotmail.com>
2024-05-30 15:07:13 +0300

    (refactor): Update ServicePointDetailsHeader component to use brand prefix and remove unused imports

--- apps/head-office/src/components/ServicePointsDetails/ServicePointsDetailsComponents/ServicePointDetailsHeader.tsx
+++ apps/head-office/src/components/ServicePointsDetails/ServicePointsDetailsComponents/ServicePointDetailsHeader.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Card from '../../Card/Card';
 import { BRAND_PREFIX } from '../../../constants/constants';
-import { RootState } from 'apps/head-office/app/redux/store';
-import { useSelector } from 'react-redux';
+import { RootState } from '../../../../app/redux/store';
 
 const ServicePointDetailsHeader: React.FC = () => {
-    const sectionPreifx = 'service-point-details-page';
+    const sectionPreifx: string = `${BRAND_PREFIX}-service-point-details-page`;
     const servicePointData = useSelector((state: RootState) => state.servicePointData.servicePointData);
 
-    const servicePointHeader = (
-        <div className={`${BRAND_PREFIX}-${sectionPreifx}-header w-full flex items-center justify-between`}>
-            <div className={`${BRAND_PREFIX}-${sectionPreifx}-header-left flex`}>
-                <h1 className={`${BRAND_PREFIX}-${sectionPreifx}-title text-2xl font-bold text-heading`}>
+    const servicePointHeader: React.ReactNode = (
+        <div className={`${sectionPreifx}-header w-full flex items-center justify-between`}>
+            <div className={`${sectionPreifx}-header-left flex`}>
+                <h1 className={`${sectionPreifx}-title text-2xl font-bold text-heading`}>
                     {servicePointData.name}
                 </h1>
             </div>
-            <div className={`${BRAND_PREFIX}-${sectionPreifx}-header-right flex`}>
+            <div className={`${sectionPreifx}-header-right flex`}>
             </div>
         </div>
     );
