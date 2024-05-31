93a35ed821fcd5bfdfec103999a5e5560daae48b
uufukttas <uufukttas@hotmail.com>
2024-05-31 09:30:43 +0300

    refactor: Improve SidebarBodyItem component structure and readability

--- apps/head-office/src/components/Sidebar/SidebarComponents/SidebarBodyItem.tsx
+++ apps/head-office/src/components/Sidebar/SidebarComponents/SidebarBodyItem.tsx
@@ -5,43 +5,45 @@ import { RootState } from '../../../../app/redux/store';
 import { BRAND_PREFIX } from '../../../../src/constants/constants';
 import type { ISidebarBodyItemProps } from '../types';
 
-const SidebarBodyItem: React.FC<ISidebarBodyItemProps> = ({ index, item, sidebarElementsLength }: ISidebarBodyItemProps) => {
-    const sidebarPrefix: string = `${BRAND_PREFIX}-sidebar`;
-    const isSidebarExpanded = useSelector((state: RootState) => state.isSidebarExpand.isSidebarExpanded);
+const SidebarBodyItem: React.FC<ISidebarBodyItemProps> =
+    ({ index, item, sidebarElementsLength }: ISidebarBodyItemProps) => {
+        const sidebarPrefix: string = `${BRAND_PREFIX}-sidebar`;
+        const isSidebarExpanded = useSelector((state: RootState) => state.isSidebarExpand.isSidebarExpanded);
 
-    return (
-        <li
-            className={`${sidebarPrefix}-list-item cursor-pointer border-gray-300 w-full ${index === sidebarElementsLength - 1
-                    ? ''
-                    : 'border-b'
-                } hover:bg-primary hover:text-white focus:bg-secondary-lighter focus:text-white`}
-            key={index}
-        >
-            <Link className={`${sidebarPrefix}-list`} href={item.link}>
-                <div className={`${sidebarPrefix}-item-container w-full flex p-4 ${isSidebarExpanded
-                    ? 'justify-start'
-                    : 'justify-center'
-                    }
+        return (
+            <li
+                className={`${sidebarPrefix}-list-item cursor-pointer border-gray-300 w-full ${
+                    index === sidebarElementsLength - 1
+                        ? ''
+                        : 'border-b'
+                    } hover:bg-primary hover:text-white focus:bg-secondary-lighter focus:text-white`}
+                key={index}
+            >
+                <Link className={`${sidebarPrefix}-list`} href={item.link}>
+                    <div className={`${sidebarPrefix}-item-container w-full flex p-4 ${isSidebarExpanded
+                        ? 'justify-start'
+                        : 'justify-center'
+                        }
                 `}
-                >
-                    <span className={`${sidebarPrefix}-item-icon`}>
-                        {item.icon}
-                    </span>
-                    <span
-                        className={`${sidebarPrefix}-item-name pl-4 ${isSidebarExpanded !== null
-                            ? isSidebarExpanded
-                                ? 'block'
-                                : 'hidden'
-                            : 'hidden'
-                            }
-                        `}
                     >
-                        {item.name}
-                    </span>
-                </div>
-            </Link>
-        </li>
-    );
-};
+                        <span className={`${sidebarPrefix}-item-icon`}>
+                            {item.icon}
+                        </span>
+                        <span
+                            className={`${sidebarPrefix}-item-name pl-4 ${isSidebarExpanded !== null
+                                ? isSidebarExpanded
+                                    ? 'block'
+                                    : 'hidden'
+                                : 'hidden'
+                                }
+                        `}
+                        >
+                            {item.name}
+                        </span>
+                    </div>
+                </Link>
+            </li>
+        );
+    };
 
 export default SidebarBodyItem;
