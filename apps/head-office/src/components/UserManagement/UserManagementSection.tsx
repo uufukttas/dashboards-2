a62ba96fa7b26fb26afc615036cf92b60036fe9e
uufukttas <uufukttas@hotmail.com>
2024-05-31 10:00:33 +0300

    (refactor): Update UserManagementSection component with filteredDropdownItems prop

--- apps/head-office/src/components/UserManagement/UserManagementSection.tsx
+++ apps/head-office/src/components/UserManagement/UserManagementSection.tsx
@@ -42,6 +42,35 @@ const UserManagementSection: React.FC = () => {
             <div className={`${userManagementPrefix}-listing-container flex items-center w-full`}>
                 <Table
                     attributeName='user-management'
+                    filteredDropdownItems={[{
+                        id: 1,
+                        isChecked: false,
+                        name: 'Isim/Soyisim',
+                        rid: null,
+                        stationFeatureType: 0,
+                        stationFeatureValue: 0,
+                    }, {
+                        id: 2,
+                        isChecked: false,
+                        name: 'Kullanici Adi',
+                        rid: null,
+                        stationFeatureType: 0,
+                        stationFeatureValue: 0,
+                    }, {
+                        id: 3,
+                        isChecked: false,
+                        name: 'Telefon',
+                        rid: null,
+                        stationFeatureType: 0,
+                        stationFeatureValue: 0,
+                    }, {
+                        id: 4,
+                        isChecked: false,
+                        name: 'Rol',
+                        rid: null,
+                        stationFeatureType: 0,
+                        stationFeatureValue: 0,
+                    }]}
                     searchedText={searchedText}
                     tableData={users}
                     tableDataCount={count}
