c81626d53109b06abf660cd1fbbb121bcbb5728b
uufukttas <uufukttas@hotmail.com>
2024-06-05 14:32:12 +0300

    (feat): added name-surname for the station permissions

--- apps/head-office/app/api/servicePointDetails/addChargePointPermission.ts
+++ apps/head-office/app/api/servicePointDetails/addChargePointPermission.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 
-const addChargePointPermission = async (permissionPhoneNumber: string, slug: string) => {
+const addChargePointPermission = async (permissionPhoneNumber: {name: string; surname: string; phoneNumber: string}, slug: string) => {
     try {
         const response = axios
         .post(
             process.env.NEXT_PUBLIC_CHARGE_POINT_USER_PERMISSION_URL || '',
             {
-                phoneNumber: permissionPhoneNumber,
+                name: permissionPhoneNumber.name,
+                surname: permissionPhoneNumber.surname,
+                phoneNumber: permissionPhoneNumber.phoneNumber,
                 stationId: Number(slug),
             },
             { headers: { 'Content-Type': 'application/json' } }
@@ -14,7 +16,9 @@ const addChargePointPermission = async (permissionPhoneNumber: string, slug: str
 
         return response;
     } catch (error) {
-        console.error(error);
+        return {
+            message: 'Error adding charge point permission',
+        }
     }
 };
 
