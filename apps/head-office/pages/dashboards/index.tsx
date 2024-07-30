e3978657f63f6234e728db240ca93ead81d6b34a
uufukttas <uufukttas@hotmail.com>
2024-07-30 11:11:55 +0300

    refactor: Move DashboardPage component from dashboards.client.tsx to DashboardPageWrapper.tsx

--- apps/head-office/pages/dashboards/index.tsx
+++ apps/head-office/pages/dashboards/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
 import { Provider } from 'react-redux';
-import Dashboards from '../dashboards/dashboards.client'
 import { store } from '../../app/redux/store';
+import Dashboards from '../../src/components/DashboardSection/DashboardPageWrapper';
 import '../../app/global.css';
 import '../../src/styles/style.css';
 
