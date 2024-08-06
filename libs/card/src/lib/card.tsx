7e0f283131f3dd8588fbaf525490b0c290dce3aa
uufukttas <uufukttas@hotmail.com>
2024-08-06 15:08:04 +0300

    refactor: Update Card component to use children prop instead of individual card components

--- libs/card/src/lib/card.tsx
+++ libs/card/src/lib/card.tsx
@@ -1,42 +1,15 @@
-import CardBody from './CardComponents/CardBody';
-import CardFooter from './CardComponents/CardFooter';
-import CardHeader from './CardComponents/CardHeader';
-export interface ICardProps {
+interface ICardProps {
   BRAND_PREFIX: string;
-  cardBody: React.JSX.Element;
-  cardBodyClassName?: string;
-  cardHeader?: React.JSX.Element;
-  cardFooter?: React.JSX.Element;
-  className?: string;
+  children: React.ReactNode;
   containerClassName?: string;
 };
 
-export function Card({ BRAND_PREFIX, cardBody, cardBodyClassName, cardFooter, cardHeader, containerClassName }: ICardProps) {
+export function Card({ BRAND_PREFIX, children, containerClassName }: ICardProps) {
   const cardPrefix: string = `${BRAND_PREFIX}-card`;
 
   return (
     <div className={`${cardPrefix}-container shadow-custom ${containerClassName}`}>
-      {
-        cardHeader && (
-          <CardHeader brandPrefix={BRAND_PREFIX}>
-            {cardHeader}
-          </CardHeader>
-        )
-      }
-      {
-        cardBody && (
-          <CardBody brandPrefix={BRAND_PREFIX} cardBodyClassName={cardBodyClassName}>
-            {cardBody}
-          </CardBody>
-        )
-      }
-      {
-        cardFooter && (
-          <CardFooter brandPrefix={BRAND_PREFIX}>
-            {cardFooter}
-          </CardFooter>
-        )
-      }
+      {children}
     </div>
   );
 };
