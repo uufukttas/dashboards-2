24cf6517e91fb885d40cfc608705c7c8fcb5d6a1
uufukttas <uufukttas@hotmail.com>
2024-08-14 11:38:06 +0300

    refactor: Update Card component to add optional styling prop

--- libs/card/src/lib/card.tsx
+++ libs/card/src/lib/card.tsx
@@ -2,13 +2,14 @@ interface ICardProps {
   BRAND_PREFIX: string;
   children: React.ReactNode;
   containerClassName?: string;
+  style?: React.CSSProperties;
 };
 
-export function Card({ BRAND_PREFIX, children, containerClassName }: ICardProps) {
+export function Card({ BRAND_PREFIX, children, containerClassName, style }: ICardProps) {
   const cardPrefix: string = `${BRAND_PREFIX}-card`;
 
   return (
-    <div className={`${cardPrefix}-container shadow-custom ${containerClassName}`}>
+    <div className={`${cardPrefix}-container shadow-custom ${containerClassName}`} style={style}>
       {children}
     </div>
   );
