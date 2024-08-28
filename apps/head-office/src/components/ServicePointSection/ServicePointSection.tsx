a12fe4b69d0c2433b1b3e7414a5e2049f785af94
uufukttas <uufukttas@hotmail.com>
2024-08-28 14:53:40 +0300

    fix: changed export button color on service points

--- apps/head-office/src/components/ServicePointSection/ServicePointSection.tsx
+++ apps/head-office/src/components/ServicePointSection/ServicePointSection.tsx
@@ -96,6 +96,7 @@ const ServicePointSection: React.FC = () => {
           <div className={`${BRAND_PREFIX}-data-table-action-button-container flex justify-center items-center`}>
             <div className={`${BRAND_PREFIX}-data-table-export-button-container mx-4`}>
               <Button
+                className={`${BRAND_PREFIX}-data-table-export-button flex justify-center items-center bg-primary rounded text-base font-semibold hover:bg-primary-lighter p-2`}
                 data-pr-tooltip="XLS"
                 icon="pi pi-file-excel"
                 rounded
