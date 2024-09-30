77722035bac750bfd20152b97f78bc1acc5a21bf
uufukttas <uufukttas@hotmail.com>
2024-09-30 11:30:25 +0300

    feat: added comments for static pages

--- apps/head-office/app/profile/page.tsx
+++ apps/head-office/app/profile/page.tsx
@@ -1,3 +1,9 @@
+/*
+    When we add api integration, it will be execute on the server side. But it is not executable on the server side.
+    There is no any api on the profile right now.
+    If there is any api integration, we can use getStaticProps or getServerSideProps.
+    End of the integration we can remove this comment and 'use client' declaration.
+*/
 'use client';
 
 import React from 'react';
