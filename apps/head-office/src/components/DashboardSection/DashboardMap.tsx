6a0436f69255f52f2bb9d701ac3f8c3dc884adf1
uufukttas <uufukttas@hotmail.com>
2024-10-01 16:03:16 +0300

    fix: fixed build errors

--- apps/head-office/src/components/DashboardSection/DashboardMap.tsx
+++ apps/head-office/src/components/DashboardSection/DashboardMap.tsx
@@ -3,13 +3,18 @@ import { GoogleMap, Marker, InfoWindow, useLoadScript, Libraries } from '@react-
 import axios from 'axios';
 import { BRAND_PREFIX } from '../../constants/constants';
 
+interface MapPinIcon {
+  url: string;
+  // other properties if needed
+}
 interface MarkerInfo {
+  mapPinIcons: MapPinIcon[];
+  stations: { id: number; name: string; mapPinIconCode: string; }[];
   lat: number;
   lon: number;
 }
 
 interface DashboardMapProps {
-  markerList: MarkerInfo[];
 };
 
 const DashboardMap: React.FC<DashboardMapProps> = () => {
@@ -25,7 +30,9 @@ const DashboardMap: React.FC<DashboardMapProps> = () => {
     libraries,
   });
 
-  const onLoad = useCallback((map: google.maps.Map) => (mapRef.current = map), []);
+  const onLoad = useCallback((map: google.maps.Map) => {
+    mapRef.current = map;
+  }, []);
 
   // const handleMarkerClick = useCallback((marker: MarkerInfo) => {
   //   setSelectedMarker(marker);
@@ -36,17 +43,17 @@ const DashboardMap: React.FC<DashboardMapProps> = () => {
   //   }
   // }, []);
 
-  const fetchAddress = async (lat: number, lng: number) => {
-    const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '';
-    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${apiKey}`);
-    const data = await response.json();
+  // const fetchAddress = async (lat: number, lng: number) => {
+  //   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '';
+  //   const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${apiKey}`);
+  //   const data = await response.json();
 
-    if (data.status === 'OK' && data.results.length > 0) {
-      setAddress(data.results[0].formatted_address);
-    } else {
-      setAddress('Address not found');
-    }
-  };
+  //   if (data.status === 'OK' && data.results.length > 0) {
+  //     setAddress(data.results[0].formatted_address);
+  //   } else {
+  //     setAddress('Address not found');
+  //   }
+  // };
 
   const getMarkerList = async () => {
     await axios.post('http://192.168.3.75:85/api/App/stations').then((response) => {
@@ -64,8 +71,10 @@ const DashboardMap: React.FC<DashboardMapProps> = () => {
 
     // İkon URL'sini belirli bir şarta göre değiştirme (örneğin kendi sunucunuzdaki ikon)
     if (iconType === '00') {
+      // @ts-ignore
       iconUrl = `http://192.168.3.75:85${markerList.mapPinIcons[0].url}`;
     } else if (iconType === '01') {
+      // @ts-ignore
       iconUrl = `http://192.168.3.75:85${markerList.mapPinIcons[1].url}`;
     }
     // İkon özelliklerini döndürme
@@ -84,7 +93,8 @@ const DashboardMap: React.FC<DashboardMapProps> = () => {
   if (!isLoaded) return <div>Loading...</div>;
 
   return (
-    markerList.stations?.length > 0 && (
+    // @ts-ignore
+    markerList?.stations?.length > 0 && (
       <div className={`${dashboardMapPrefix}-container w-full px-2 my-4`}>
         <GoogleMap
           center={{ lat: 39.92504, lng: 30.83709 }} // Heart of Turkey - Ankara Anitkabir
@@ -97,14 +107,17 @@ const DashboardMap: React.FC<DashboardMapProps> = () => {
           zoom={7}
           onLoad={onLoad}
         >
-          {markerList.stations.map((marker, index) => (
-            <Marker
-              key={index}
-              position={{ lat: marker.lat, lng: marker.lon }}
-              // onClick={() => handleMarkerClick(marker)}
-              icon={setMarkerIcon(marker.mapPinIconCode)} // Özel ikon tipi burada belirleniyor
-            />
-          ))}
+          {
+            // @ts-ignore
+            markerList.stations.map((marker, index) => (
+              <Marker
+                key={index}
+                position={{ lat: marker.lat, lng: marker.lon }}
+                // onClick={() => handleMarkerClick(marker)}
+                icon={setMarkerIcon(marker.mapPinIconCode)} // Özel ikon tipi burada belirleniyor
+              />
+            ))
+          }
           {
             selectedMarker && (
               <InfoWindow
