3f7c46d729568b1fe4f6d7787d3d8d24c55cf06a
uufukttas <uufukttas@hotmail.com>
2024-10-02 14:13:28 +0300

    feat: added info window on google maps

--- apps/head-office/src/components/DashboardSection/DashboardMap.tsx
+++ apps/head-office/src/components/DashboardSection/DashboardMap.tsx
@@ -1,17 +1,24 @@
-import React, { useCallback, useRef, useState, useEffect } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { GoogleMap, Marker, InfoWindow, useLoadScript, Libraries } from '@react-google-maps/api';
 import axios from 'axios';
+import { IoCloseCircle } from "react-icons/io5";
 import { BRAND_PREFIX } from '../../constants/constants';
 
 interface MapPinIcon {
   url: string;
-  // other properties if needed
-}
-interface MarkerInfo {
-  mapPinIcons: MapPinIcon[];
-  stations: { id: number; name: string; mapPinIconCode: string; }[];
+};
+
+interface Station {
+  id: number;
+  name: string;
+  mapPinIconCode: string;
   lat: number;
   lon: number;
+}
+
+interface MarkerInfo {
+  mapPinIcons: MapPinIcon[];
+  stations: Station[];
 }
 
 interface DashboardMapProps {
@@ -21,8 +28,11 @@ const DashboardMap: React.FC<DashboardMapProps> = () => {
   const dashboardMapPrefix: string = `${BRAND_PREFIX}-dashboard-map`;
   const libraries: Libraries = ["places"];
   const mapRef = useRef<google.maps.Map | null>(null);
-  const [selectedMarker, setSelectedMarker] = useState<MarkerInfo | null>(null);
-  const [address, setAddress] = useState<string | null>(null);
+  const [selectedMarker, setSelectedMarker] = useState<Station | null>(null);
+  const [address, setAddress] = useState({
+    address: "",
+    name: ""
+  });
   const [markerList, setMarkerList] = useState<MarkerInfo[]>([]);
 
   const { isLoaded, loadError } = useLoadScript({
@@ -34,66 +44,53 @@ const DashboardMap: React.FC<DashboardMapProps> = () => {
     mapRef.current = map;
   }, []);
 
-  // const handleMarkerClick = useCallback((marker: MarkerInfo) => {
-  //   setSelectedMarker(marker);
-  //   fetchAddress(marker.lat, marker.lon);
-  //   if (mapRef.current) {
-  //     mapRef.current.panTo({ lat: marker.lat, lng: marker.lon });
-  //     mapRef.current.setZoom(14);
-  //   }
-  // }, []);
+  const handleMarkerClick = useCallback((marker: Station) => {
+    setSelectedMarker(marker);
+    fetchAddress(marker);
+    if (mapRef.current) {
+      mapRef.current.panTo({ lat: marker.lat, lng: marker.lon });
+    }
+  }, []);
 
-  // const fetchAddress = async (lat: number, lng: number) => {
-  //   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '';
-  //   const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${apiKey}`);
-  //   const data = await response.json();
+  const fetchAddress = async (marker: Station) => {
+    const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '';
+    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${marker.lat},${marker.lon}&key=${apiKey}`);
+    const data = await response.json();
 
-  //   if (data.status === 'OK' && data.results.length > 0) {
-  //     setAddress(data.results[0].formatted_address);
-  //   } else {
-  //     setAddress('Address not found');
-  //   }
-  // };
-
-  const getMarkerList = async () => {
-    await axios.post('http://192.168.3.75:85/api/App/stations').then((response) => {
-      setMarkerList(response.data.result);
-    });
+    if (data.status === 'OK' && data.results.length > 0) {
+      setAddress({
+        address: data.results[0].formatted_address,
+        name: marker.name
+      });
+    } else {
+      setAddress({
+        address: 'Address not found',
+        name: marker.name
+      });
+    };
   };
 
-  // `window.google`'ın yüklenmesini kontrol ederek `TypeError` hatasını önlüyoruz
   const setMarkerIcon = (iconType: string) => {
-    if (!isLoaded || !window.google || !window.google.maps) return undefined;
+    if (!isLoaded || !window.google || !window.google.maps || !markerList) return undefined;
 
-    // Dinamik olarak özel ikon URL'si belirleme
     const baseIconUrl = `https://maps.google.com/mapfiles/ms/icons/`;
     let iconUrl = `${baseIconUrl}${iconType}-dot.png`;
 
-    // İkon URL'sini belirli bir şarta göre değiştirme (örneğin kendi sunucunuzdaki ikon)
     if (iconType === '00') {
-      // @ts-ignore
       iconUrl = `http://192.168.3.75:85${markerList.mapPinIcons[0].url}`;
     } else if (iconType === '01') {
-      // @ts-ignore
       iconUrl = `http://192.168.3.75:85${markerList.mapPinIcons[1].url}`;
     }
-    // İkon özelliklerini döndürme
     return {
       url: iconUrl,
       scaledSize: new window.google.maps.Size(40, 40),
     };
   };
 
-  useEffect(() => {
-    getMarkerList();
-  }, []);
-
-
   if (loadError) return <div>Error loading map</div>;
-  if (!isLoaded) return <div>Loading...</div>;
+  if (!isLoaded || !markerList) return <div>Loading...</div>;
 
   return (
-    // @ts-ignore
     markerList?.stations?.length > 0 && (
       <div className={`${dashboardMapPrefix}-container w-full px-2 my-4`}>
         <GoogleMap
@@ -108,12 +105,11 @@ const DashboardMap: React.FC<DashboardMapProps> = () => {
           onLoad={onLoad}
         >
           {
-            // @ts-ignore
             markerList.stations.map((marker, index) => (
               <Marker
                 key={index}
                 position={{ lat: marker.lat, lng: marker.lon }}
-                // onClick={() => handleMarkerClick(marker)}
+                onClick={() => handleMarkerClick(marker)}
                 icon={setMarkerIcon(marker.mapPinIconCode)} // Özel ikon tipi burada belirleniyor
               />
             ))
@@ -124,15 +120,19 @@ const DashboardMap: React.FC<DashboardMapProps> = () => {
                 position={{ lat: selectedMarker.lat, lng: selectedMarker.lon }}
                 onCloseClick={() => {
                   setSelectedMarker(null);
-                  setAddress(null);
+                  setAddress({
+                    address: "",
+                    name: ""
+                  });
                 }}
               >
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <div style={{ marginRight: '8px' }}>
-                    <p style={{ margin: 0 }}>{address || 'Loading address...'}</p>
+                <div className={`${dashboardMapPrefix}-information-window-container flex items-start justify-center`}>
+                  <div className={`${dashboardMapPrefix}-information-window-content-container mr-2`}>
+                    <p>{address.name}</p>
+                    <p style={{ margin: 0 }}>{address.address}</p>
                   </div>
-                  <button onClick={() => setSelectedMarker(null)} style={{ marginLeft: 'auto', fontSize: '20px', fontWeight: 'bolder' }}>
-                    X
+                  <button onClick={() => setSelectedMarker(null)}>
+                    <IoCloseCircle className={`${dashboardMapPrefix} text-lg`} />
                   </button>
                 </div>
               </InfoWindow>
