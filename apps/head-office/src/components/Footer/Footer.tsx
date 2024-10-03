7665209972fd27f42a0c5c0df870290d7c8c3218
uufukttas <uufukttas@hotmail.com>
2024-10-03 15:10:36 +0300

    fix: refactor Footer components

--- apps/head-office/src/components/Footer/Footer.tsx
+++ apps/head-office/src/components/Footer/Footer.tsx
@@ -7,9 +7,13 @@ const Footer: React.FC<IFooterProps> = ({ className }: IFooterProps) => {
   const footerPrefix: string = `${BRAND_PREFIX}-footer`;
 
   return (
-    <div className={`${footerPrefix}-container w-full h-[40px] justify-between border-t border-gray-300 bg-background top-0 z-10 sticky ${className}`}>
+    <div
+      className={`${footerPrefix}-container w-full h-[40px] justify-between border-t border-gray-300 bg-background top-0 z-10 sticky ${className}`}
+    >
       <div className={`${footerPrefix}-content w-full`}>
-        <span className={`${footerPrefix}-content-text flex justify-end items-center px-8 text-xs`}>© 2024 - All rights reserved</span>
+        <span className={`${footerPrefix}-content-text flex justify-end items-center px-8 text-xs`}>
+          © 2024 - All rights reserved
+        </span>
       </div>
     </div>
   );
