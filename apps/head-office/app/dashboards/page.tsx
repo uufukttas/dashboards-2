bb6a5f8e51147c2e6209ae31b8673a6456924f01
uufukttas <uufukttas@hotmail.com>
2024-10-14 15:35:14 +0300

    fix: changed render type from server to client with use client rules

--- apps/head-office/app/dashboards/page.tsx
+++ apps/head-office/app/dashboards/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import { Metadata } from 'next';
 import DashboardsClient from './ClientComponents/DashboardClient';
@@ -5,9 +7,9 @@ import { getMapStationsList } from '../api/dashboards';
 import '../../app/global.css';
 import '../../src/styles/style.css';
 
-export const metadata: Metadata = {
-  title: 'Dashboards | Sharz.net',
-};
+// export const metadata: Metadata = {
+//   title: 'Dashboards | Sharz.net',
+// };
 
 const DashboardsPage = async () => {
   const { description, result, success } = await getMapStationsList();
