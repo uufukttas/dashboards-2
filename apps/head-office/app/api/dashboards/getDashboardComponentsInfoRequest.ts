3943c6d2020eec4c109bfdac319dcc2a56fd5224
uufukttas <uufukttas@hotmail.com>
2024-10-24 09:13:13 +0300

    feat: updated dashboardItem Request with param

--- apps/head-office/app/api/dashboards/getDashboardComponentsInfoRequest.ts
+++ apps/head-office/app/api/dashboards/getDashboardComponentsInfoRequest.ts
@@ -4,7 +4,7 @@ const getDashboardComponentInfoRequest = async (dashboardPageCode: string) => {
     try {
         const response = await axios.get(
             `${process.env.NEXT_PUBLIC_BASE_URL
-            }/Dashboard/GetDashboardItems/Dashboard/GetDashboardItems/${dashboardPageCode}`,
+            }/Dashboard/GetDashboardItems?Pagecode=${dashboardPageCode}`,
             {
                 headers: {
                     'Authorization': `Bearer ${localStorage.getItem('token') || ''}`
