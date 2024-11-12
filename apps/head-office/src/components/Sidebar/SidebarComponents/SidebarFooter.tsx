cbd23e7a721f36f2a23bfc3b2c2805397e8dddd9
uufukttas <uufukttas@hotmail.com>
2024-11-12 08:54:13 +0300

    head-office#feat: added logout action with sidebar

--- apps/head-office/src/components/Sidebar/SidebarComponents/SidebarFooter.tsx
+++ apps/head-office/src/components/Sidebar/SidebarComponents/SidebarFooter.tsx
@@ -8,14 +8,16 @@ const SidebarFooter: React.FC = () => {
     const sidebarFooterPrefix: string = `${BRAND_PREFIX}-sidebar-footer`;
 
     return (
-        <div className={`${sidebarFooterPrefix}-sidebar-footer-container mt-auto`}>
-            <hr className="mb-3 mx-3 border-top-1 border-none surface-border" />
+        <div className={`${sidebarFooterPrefix}-sidebar-footer-container mt-auto flex items-center justify-between`}>
             <p className="m-3 flex align-items-center justify-center cursor-pointer p-3 gap-2 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple">
                 <span>
                     © 2024
                 </span>
-                <Image src="/evslogo.jpg" alt='logo' width={30} height={30} className='object-cover'/>
+                <Image src="/evslogo.jpg" alt='logo' width={30} height={30} className='object-cover' />
             </p>
+            <Link href='/' onClick={() => { localStorage.setItem('token', '') }}>
+                <FaDoorOpen className='mx-8 text-2xl' />
+            </Link>
         </div>
     );
 };
