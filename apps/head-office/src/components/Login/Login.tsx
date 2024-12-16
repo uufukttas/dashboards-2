8c8f4d6e6509ab8e18261b806d0efd97b02924ab
uufukttas <uufukttas@hotmail.com>
2024-12-16 08:30:27 +0300

    fix: change dropdown element to the pure dropdown select element

--- apps/head-office/src/components/Login/Login.tsx
+++ apps/head-office/src/components/Login/Login.tsx
@@ -9,34 +9,39 @@ import './Login.css';
 import CardBody from './LoginCardComponents/CardBody';
 import CardFooter from './LoginCardComponents/CardFooter';
 import CardHeader from './LoginCardComponents/CardHeader';
+import BaseSelect from '../Base/BaseSelect';
+import { useForm } from 'react-hook-form';
 
 const Login: React.FC = () => {
   const loginPrefix: string = `${BRAND_PREFIX}-login`;
   const dispatch = useDispatch();
   const { data: languages } = useGetLanaugeListQuery({});
+  const form = useForm();
 
   return (
-    <div className={`${loginPrefix}-side-container flex flex-col justify-between w-full px-8 py-4 h-screen`}>
-      <div className={`${loginPrefix}-language-option-container flex justify-end`}>
-        <Dropdown
-          className={`${loginPrefix}-language-dropdown block w-1/2 p-2.5 border rounded-lg text-text text-sm focus:ring-primary focus:border-primary`}
-          id={`${loginPrefix}-language-dropdown`}
-          items={languages}
-          name={`${loginPrefix}-language-dropdown`}
-          onChange={(event) => dispatch(setSelectedLanguage(Number(event.target.value)))}
-        />
+    languages && (
+      <div className={`${loginPrefix}-side-container flex flex-col justify-between w-full px-8 py-4 h-screen`}>
+        <div className={`${loginPrefix}-language-option-container flex justify-end`}>
+          <BaseSelect
+            className={`${loginPrefix}-language-dropdown block w-1/2 p-2.5 border rounded-lg text-text text-sm focus:ring-primary focus:border-primary`}
+            defaultValue={languages[0].id}
+            form={form}
+            id={`${loginPrefix}-language-dropdown`}
+            items={languages}
+            name={`${loginPrefix}-language-dropdown`}/>
+        </div>
+        <div className={`${loginPrefix}-card-form-container flex justify-center`}>
+          <Card BRAND_PREFIX={BRAND_PREFIX} containerClassName={`w-full md:w-1/2 lg:w-full p-8 bg-white`}>
+            <CardHeader />
+            <CardBody />
+            <CardFooter />
+          </Card>
+        </div>
+        <div className={`${loginPrefix}-footer-container`}>
+          <p className={`${loginPrefix}-footer`}></p>
+        </div>
       </div>
-      <div className={`${loginPrefix}-card-form-container flex justify-center`}>
-        <Card BRAND_PREFIX={BRAND_PREFIX} containerClassName={`w-full md:w-1/2 lg:w-full p-8 bg-white`}>
-          <CardHeader />
-          <CardBody />
-          <CardFooter />
-        </Card>
-      </div>
-      <div className={`${loginPrefix}-footer-container`}>
-        <p className={`${loginPrefix}-footer`}></p>
-      </div>
-    </div>
+    )
   );
 };
 
